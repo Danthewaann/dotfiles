39399fc26c69fa5ae2047bfcf09043a452ec2e8b
dcrblack <danielcrblack@gmail.com>
2023-02-02 16:17:39 +0000

    Added tmux installer

---
+++ tmux/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source "$SCRIPT_DIR"/../common
+
+if [[ $OSTYPE == "darwin"* ]]; then
+    brew install tmux
+else
+    sudo apt-get install -y tmux
+fi
+
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
