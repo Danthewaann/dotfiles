9ef8d9d38828728cff48f4f1592b7bc717077e76
dcrblack <danielcrblack@gmail.com>
2023-02-03 14:18:04 +0000

    Install vim 9 + add virtualText to coc

--- vim/install.sh
+++ vim/install.sh
@@ -6,17 +6,26 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "$SCRIPT_DIR"/../common
 
+# Install vim 9
+if [[ $OSTYPE == "darwin"* ]]; then
+    brew install vim
+else
+    sudo add-apt-repository -y ppa:jonathonf/vim
+    sudo apt-get install -y vim
+fi
+
 # Install Vundle vim plugin manager
 if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    # Workaround from https://github.com/neoclide/coc.nvim/issues/3258#issuecomment-1056660012
-    cd ~/.vim/bundle/coc.nvim || exit
-    git checkout release
-    cd -
 fi
 
 # Install plugins
 vim --not-a-term +PluginInstall +qall > /dev/null 2>&1
+
+# Workaround from https://github.com/neoclide/coc.nvim/issues/3258#issuecomment-1056660012
+cd ~/.vim/bundle/coc.nvim || exit
+git checkout release
+cd -
 
 # vimspector debugpy post-install step
 vim --not-a-term "+VimspectorInstall debugpy" +qall > /dev/null 2>&1
