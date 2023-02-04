3166f60055f7c7bebd4e4f8e9d7f9647483affed
dcrblack <danielcrblack@gmail.com>
2023-02-04 13:44:42 +0000

    Fixed shellcheck errors + install bash lsp

--- node/install.sh
+++ node/install.sh
@@ -4,15 +4,22 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# shellcheck source=/dev/null
 source "$SCRIPT_DIR"/../common
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
+# shellcheck disable=SC1091
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# shellcheck disable=SC1091
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Install node and npm
 nvm install "$NODE_VERSION"
+nvm alias default "$NODE_VERSION"
+nvm use default
 
+# Install bash language server
+npm -i -g bash-language-server
--- tmux/install.sh
+++ tmux/install.sh
@@ -4,6 +4,7 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# shellcheck source=/dev/null
 source "$SCRIPT_DIR"/../common
 
 if [[ $OSTYPE == "darwin"* ]]; then
