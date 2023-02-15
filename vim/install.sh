de8f5bb54c54b3b499eb556a70044843b3824904
dcrblack <danielcrblack@gmail.com>
2023-02-15 11:03:48 +0000

    Add git-delta + bat

--- vim/install.sh
+++ vim/install.sh
@@ -47,8 +47,30 @@ run_command "installing go binaries for vim-go" "installed go binaries for vim-g
 if [[ $OSTYPE == "darwin"* ]]; then
     run_command "installing vim plugin dependencies" "installed vim plugin dependencies" \
         "brew install the_silver_searcher fzf shellcheck"
+
+    run_command "installing git-delta" "installed git-delta" \
+        "brew install git-delta"
+
+    run_command "installing bat" "installed bat" \
+        "brew install bat"
 else
     run_command "installing vim plugin dependencies" "installed vim plugin dependencies" \
         "sudo apt-get install -y silversearcher-ag fzf shellcheck"
+
+    if [[ ! -f "$SCRIPT_DIR/git-delta.deb" ]]; then
+        run_command "downloading git-delta" "downloaded git-delta"\
+            "wget -O $SCRIPT_DIR/git-delta.deb https://github.com/dandavison/delta/releases/download/0.15.1/git-delta_0.15.1_amd64.deb"
+    fi
+
+    run_command "installing git-delta" "installed git-delta" \
+        "sudo dpkg -i $SCRIPT_DIR/git-delta.deb"
+
+    if [[ ! -f "$SCRIPT_DIR/bat.deb" ]]; then
+        run_command "downloading bat" "downloaded bat"\
+            "wget -O $SCRIPT_DIR/bat.deb https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-musl_0.22.1_amd64.deb"
+    fi
+
+    run_command "installing bat" "installed bat" \
+        "sudo dpkg -i $SCRIPT_DIR/bat.deb"
 fi
 
