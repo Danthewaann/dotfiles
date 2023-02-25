3cd21d838d330d065dbe9329c29b9fd87770e69f
dcrblack <danielcrblack@gmail.com>
2023-02-25 11:08:57 +0000

    Fix macos theme zip file check

--- tmux/install.sh
+++ tmux/install.sh
@@ -11,12 +11,13 @@ if [[ $OSTYPE == "darwin"* ]]; then
     run_command "installing tmux" "installed tmux" \
         "brew install tmux"
 
-    if [[ ! -f $SCRIPT_DIR/onedark.itermcolors ]]; then
+    if [[ ! -f $SCRIPT_DIR/onedark_macos.zip ]]; then
         run_command "downloading one dark MACOS terminal theme" "downloaded one dark MACOS terminal theme" \
             "curl -L -o $SCRIPT_DIR/onedark_macos.zip https://github.com/nathanbuchar/atom-one-dark-terminal/releases/download/v1.0.3/terminal.zip"
+
+        run_command "unpacking one dark MACOS terminal theme" "unpacked one dark MACOS terminal theme" \
+            "unzip -o $SCRIPT_DIR/onedark_macos.zip -d $SCRIPT_DIR/onedark_macos"
     fi
-    run_command "unpacking one dark MACOS terminal theme" "unpacked one dark MACOS terminal theme" \
-        "unzip -o $SCRIPT_DIR/onedark_macos.zip -d $SCRIPT_DIR/onedark_macos"
 else
     run_command "installing tmux" "installed tmux" \
         "sudo apt-get install -y tmux"
