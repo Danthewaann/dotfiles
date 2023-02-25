5ffa040a7c892a9a548f645c380cfee3175b977b
dblack <dblack@dbdesktop.Home>
2023-02-25 21:32:51 +0000

    Add rust installer

---
+++ 010_rust/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# shellcheck source=/dev/null
+source "$SCRIPT_DIR"/../common
+
+run_command "installing rust" "installed rust" \
+    "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
+
+run_command "installing rust-analyzer" "installed rust-analyzer" \
+    "$(which rustup) component add rust-analyzer"
+
+info "linking rust-analyzer"
+link_file "$(rustup which --toolchain stable rust-analyzer)" "$HOME/.cargo/bin/rust-analyzer"
