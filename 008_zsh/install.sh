c7cf62bf63307bd15872230058f3399a7124e9dc
dcrblack <danielcrblack@gmail.com>
2023-02-26 17:22:48 +0000

    Adjust KeyRepeat on macos

--- 008_zsh/install.sh
+++ 008_zsh/install.sh
@@ -9,9 +9,9 @@ source "$SCRIPT_DIR"/../common
 
 if [[ $OSTYPE == "darwin"* ]]; then
     run_command "setting up initial key-repeat" "set up initial key-repeat" \
-        "defaults write -g InitialKeyRepeat -int 10" \  # normal minimum is 15 (225 ms)
+        "defaults write -g InitialKeyRepeat -int 15" \
     run_command "setting up key-repeat" "set up key-repeat" \
-        "defaults write -g KeyRepeat -int 1" # normal minimum is 2 (30 ms)
+        "defaults write -g KeyRepeat -int 2"
 else
     run_command "setting up key-repeat" "set up key-repeat" \
         "gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30" \
