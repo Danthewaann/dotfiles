3a947788ffdec39d7794369dd449df83aa0fadf1
dcrblack <danielcrblack@gmail.com>
2023-03-02 20:12:47 +0000

    Add git installer with github cli

---
+++ 001_git/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# shellcheck source=/dev/null
+source "$SCRIPT_DIR"/../common
+
+if [[ $OSTYPE == "darwin"* ]]; then
+    run_command "installing github cli" "installed github cli" \
+        "brew install gh"
+else
+    if [[ ! -f "$SCRIPT_DIR/gh_2.23.0_linux_amd64.deb" ]]; then
+        run_command "downloading github cli" "downloaded github cli" \
+            "wget -O $SCRIPT_DIR/gh_2.23.0_linux_amd64.deb \\
+            https://github.com/cli/cli/releases/download/v2.23.0/gh_2.23.0_linux_amd64.deb"
+    fi
+
+    run_command "installing github cli" "installed github cli" \
+        "sudo dpkg -i $SCRIPT_DIR/gh_2.23.0_linux_amd64.deb"
+fi
