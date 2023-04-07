0c035190682c6ea544df8296c414f4b84f1ca6ae
dcrblack <daniel.black@slicelife.com>
2023-04-07 18:08:49 +0100

    Add kitty terminal setup and config

--- 007_tmux/install.sh
+++ 007_tmux/install.sh
@@ -11,24 +11,21 @@ if [[ $OSTYPE == "darwin"* ]]; then
     run_command "installing tmux" "installed tmux" \
         "brew install tmux"
 
-    run_command "installing iterm2" "installed iterm2" \
-        "brew install --cask iterm2"
-
-    if [[ ! -f $SCRIPT_DIR/onedark.itermcolors ]]; then
-        run_command "downloading one dark MACOS iterm2 theme" "downloaded one dark MACOS iterm2 theme" \
-            "curl -L -o $SCRIPT_DIR/onedark.itermcolors https://raw.githubusercontent.com/one-dark/iterm-one-dark-theme/main/One%20Dark.itermcolors"
-    fi
+    run_command "installing kitty" "installed kitty" \
+        "brew install --cask kitty"
 else
     run_command "installing tmux" "installed tmux" \
         "sudo apt-get install -y tmux"
 
-    # From https://github.com/denysdovhan/one-gnome-terminal
-    if [[ ! -f $SCRIPT_DIR/.gnome_installed_one_dark_theme ]]; then
-        run_command "installing one dark GNOME terminal theme" "installed one dark GNOME terminal theme" \
-            "curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh | bash"
-        touch "$SCRIPT_DIR/.gnome_installed_one_dark_theme"
-    fi
+    run_command "installing kitty" "installed kitty" \
+        "sudo apt-get install -y kitty"
 fi
+
+info "linking OneDark.conf colour theme for kitty"
+link_file "$SCRIPT_DIR/OneDark.conf" "$HOME/.config/kitty/OneDark.conf"
+
+info "linking kitty.conf"
+link_file "$SCRIPT_DIR/kitty.conf" "$HOME/.config/kitty/kitty.conf"
 
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
     run_command "cloning tpm to -> ~/.tmux/plugins/tpm" "cloned tpm to -> ~/.tmux/plugins/tpm" \
