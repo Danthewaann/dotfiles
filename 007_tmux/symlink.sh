8a188052faff977a8e827b248603d10c5c20c8fc
dcrblack <daniel.black@slicelife.com>
2023-04-10 19:01:42 +0100

    Make sure to symlink .tmux.conf

--- 007_tmux/symlink.sh
+++ 007_tmux/symlink.sh
@@ -7,6 +7,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck source=/dev/null
 source "$SCRIPT_DIR"/../common
 
+echo "$SCRIPT_DIR/.tmux.conf" "$HOME/.tmux.conf"
+
 echo "$SCRIPT_DIR/OneDark.conf" "$HOME/.config/kitty/OneDark.conf"
 
 echo "$SCRIPT_DIR/kitty.conf" "$HOME/.config/kitty/kitty.conf"
