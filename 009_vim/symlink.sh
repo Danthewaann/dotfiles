7a773a56c3ef73151c749e41ac6028d630edc3ea
dcrblack <daniel.black@slicelife.com>
2023-04-22 08:23:26 +0100

    Added ftplugin/python.vim for adding zz to navigation binds

--- 009_vim/symlink.sh
+++ 009_vim/symlink.sh
@@ -16,9 +16,14 @@ echo "$SCRIPT_DIR/.vimspector.json" "$HOME/.vimspector.json"
 mkdir -p "$HOME/.config/clangd"
 echo "$SCRIPT_DIR/clangd_config.yaml" "$HOME/.config/clangd/config.yaml"
 
+mkdir -p "$HOME/.vim/autoload"
 for d in "$SCRIPT_DIR"/autoload/*; do
     if [[ -d "$d" ]]; then
         echo "$d" "$HOME/.vim/autoload/$(basename "$d")"
     fi
 done
 
+mkdir -p "$HOME/.vim/ftplugin"
+for f in "$SCRIPT_DIR"/ftplugin/*; do
+    echo "$f" "$HOME/.vim/ftplugin/$(basename "$f")"
+done
