4cbaab8a9178730a02a4643c364749ba7631d15d
dcrblack <daniel.black@slicelife.com>
2023-05-24 21:57:44 +0100

    Make sure $HOME/.config/kitty directory exists

--- 007_tmux/symlink.sh
+++ 007_tmux/symlink.sh
@@ -9,6 +9,8 @@ source "$SCRIPT_DIR"/../common
 
 echo "$SCRIPT_DIR/.tmux.conf" "$HOME/.tmux.conf"
 
+mkdir -p "$HOME/.config/kitty"
+
 echo "$SCRIPT_DIR/OneDark.conf" "$HOME/.config/kitty/OneDark.conf"
 
 echo "$SCRIPT_DIR/kitty.conf" "$HOME/.config/kitty/kitty.conf"
