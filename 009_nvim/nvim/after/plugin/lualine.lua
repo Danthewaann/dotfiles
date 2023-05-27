15cd9d7d7de946967cf3b6c32e7f798ecd8ad0b7
dcrblack <daniel.black@slicelife.com>
2023-05-27 14:20:31 +0100

    Disable fugitive in lualine

--- 009_nvim/nvim/after/plugin/lualine.lua
+++ 009_nvim/nvim/after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup({
     options = {
         disabled_filetypes = {
-            statusline = { "NvimTree" }
+            statusline = { "NvimTree", "fugitive" }
         }
     },
     sections = {
