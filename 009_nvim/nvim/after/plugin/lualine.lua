c258155979238d6921b528e4d7fcbd2d86eafbf4
dcrblack <daniel.black@slicelife.com>
2023-05-27 14:21:27 +0100

    Disable dbui in lualine

--- 009_nvim/nvim/after/plugin/lualine.lua
+++ 009_nvim/nvim/after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup({
     options = {
         disabled_filetypes = {
-            statusline = { "NvimTree", "fugitive" }
+            statusline = { "NvimTree", "fugitive", "dbui" }
         }
     },
     sections = {
