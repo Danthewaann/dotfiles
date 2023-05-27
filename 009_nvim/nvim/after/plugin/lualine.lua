a25afe3ca084c4a3085e696441bfbf573bfa87c0
dcrblack <daniel.black@slicelife.com>
2023-05-27 14:15:16 +0100

    Use git gutter colours in lualine

--- 009_nvim/nvim/after/plugin/lualine.lua
+++ 009_nvim/nvim/after/plugin/lualine.lua
@@ -11,9 +11,9 @@ require('lualine').setup({
                 colored = true, -- Displays a colored diff status if set to true
                 diff_color = {
                     -- Same color values as the general color option can be used here.
-                    added    = 'DiffAdd',    -- Changes the diff's added color
-                    modified = 'DiffChange', -- Changes the diff's modified color
-                    removed  = 'DiffDelete', -- Changes the diff's removed color you
+                    added    = 'GitGutterAdd',    -- Changes the diff's added color
+                    modified = 'GitGutterChange', -- Changes the diff's modified color
+                    removed  = 'GitGutterDelete', -- Changes the diff's removed color you
                 },
                 symbols = {added = '+', modified = '~', removed = '-'}, -- Changes the symbols used by the diff.
                 source = nil, -- A function that works as a data source for diff.
