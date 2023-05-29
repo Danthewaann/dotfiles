d213516725a49a7866f923487baa903f4c4e3533
dcrblack <daniel.black@slicelife.com>
2023-05-29 11:40:43 +0100

    Use slanted components for lualine and tabline

--- 009_nvim/nvim/after/plugin/lualine.lua
+++ 009_nvim/nvim/after/plugin/lualine.lua
@@ -2,25 +2,14 @@ require('lualine').setup({
     options = {
         disabled_filetypes = {
             statusline = { "NvimTree", "fugitive", "dbui" }
-        }
+        },
+        component_separators = { left = '╲', right = '╱' },
+        section_separators = { left = '', right = '' },
     },
     sections = {
         lualine_b = {
-            {
-                'diff',
-                colored = true, -- Displays a colored diff status if set to true
-                diff_color = {
-                    -- Same color values as the general color option can be used here.
-                    added    = 'GitGutterAdd',    -- Changes the diff's added color
-                    modified = 'GitGutterChange', -- Changes the diff's modified color
-                    removed  = 'GitGutterDelete', -- Changes the diff's removed color you
-                },
-                symbols = {added = '+', modified = '~', removed = '-'}, -- Changes the symbols used by the diff.
-                source = nil, -- A function that works as a data source for diff.
-                -- It must return a table as such:
-                --   { added = add_count, modified = modified_count, removed = removed_count }
-                -- or nil on failure. count <= 0 won't be displayed.
-            }
+            'diff',
+            'diagnostics'
         },
     }
 })
