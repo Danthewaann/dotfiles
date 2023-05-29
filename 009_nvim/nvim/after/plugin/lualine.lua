2185a8a71af8d079edffb3b0df93ab3f7f01cf34
dcrblack <daniel.black@slicelife.com>
2023-05-29 14:15:17 +0100

    Add NvimTree and fugitive extensions to lualine

--- 009_nvim/nvim/after/plugin/lualine.lua
+++ 009_nvim/nvim/after/plugin/lualine.lua
@@ -1,11 +1,12 @@
 require('lualine').setup({
     options = {
         disabled_filetypes = {
-            statusline = { "NvimTree", "fugitive", "dbui" }
+            statusline = { 'dbui' }
         },
         component_separators = { left = '╲', right = '╱' },
         section_separators = { left = '', right = '' },
     },
+    extensions = { 'fugitive', 'nvim-tree' },
     sections = {
         lualine_b = {
             'diff',
