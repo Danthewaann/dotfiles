456c359f8b9222ecf83a4cc29bcda772cb0e5422
dcrblack <daniel.black@slicelife.com>
2023-06-01 20:35:53 +0100

    Move config nvim-tree to nvim-tree.lua

--- 009_nvim/nvim/after/plugin/nvim-tree.lua
+++ 009_nvim/nvim/after/plugin/nvim-tree.lua
@@ -1,3 +1,16 @@
+require("nvim-tree").setup({
+    git = {
+        enable = true,
+        ignore = false,
+    },
+    view = {
+        width = 35,
+    },
+    renderer = {
+        full_name = true,
+    },
+})
+
 vim.keymap.set("n", "<C-n>", "<cmd> silent NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>nf", "<cmd> silent NvimTreeFindFile<CR>")
 
