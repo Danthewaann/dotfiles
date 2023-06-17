e6f80a74a39d2fda89502e7e59e97b1da446f190
dcrblack <daniel.black@slicelife.com>
2023-06-17 12:19:25 +0100

    Use <C-p> to exit terminal mode

--- 009_nvim/nvim/lua/mystuff/remap.lua
+++ 009_nvim/nvim/lua/mystuff/remap.lua
@@ -93,11 +93,10 @@ vim.keymap.set("t", "<C-w>9", "<C-\\><C-n> <cmd> silent tabn9<CR>")
 vim.keymap.set("t", "<C-w>0", "<C-\\><C-n> <cmd> silent tablast<CR>")
 
 -- Enter normal-mode in nvim terminal
-vim.keymap.set("t", "<C-w><C-x>", "<C-\\><C-n>")
+vim.keymap.set("t", "<C-q>", "<C-\\><C-n>")
 
 -- Exit the current window
 vim.keymap.set("n", "<C-q>", "<cmd> silent q<CR>")
-vim.keymap.set("t", "<C-q>", "<C-\\><C-n> <cmd> silent q<CR>")
 
 -- Treat Ctrl+C exactly like <Esc> in insert mode
 vim.keymap.set("i", "<C-c>", "<Esc>")
