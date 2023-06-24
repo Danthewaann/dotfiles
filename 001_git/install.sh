5eee0c4a63ea5ece7e8624edec24c5bc23b78f7b
dcrblack <danielcrblack@gmail.com>
2023-06-24 10:38:18 +0100

    Update github cli version on linux

--- 001_git/install.sh
+++ 001_git/install.sh
@@ -11,14 +11,14 @@ if [[ $OSTYPE == "darwin"* ]]; then
     run_command "installing github cli" \
         "brew install gh"
 else
-    if [[ ! -f "$SCRIPT_DIR/gh_2.23.0_linux_amd64.deb" ]]; then
+    if [[ ! -f "$SCRIPT_DIR/gh_2.31.0_linux_amd64.deb" ]]; then
         run_command "downloading github cli" \
-            "wget -O $SCRIPT_DIR/gh_2.23.0_linux_amd64.deb \\
-            https://github.com/cli/cli/releases/download/v2.23.0/gh_2.23.0_linux_amd64.deb"
+            "wget -O $SCRIPT_DIR/gh_2.31.0_linux_amd64.deb \\
+            https://github.com/cli/cli/releases/download/v2.31.0/gh_2.31.0_linux_amd64.deb"
     fi
 
     run_command "installing github cli" \
-        "sudo dpkg -i $SCRIPT_DIR/gh_2.23.0_linux_amd64.deb"
+        "sudo dpkg -i $SCRIPT_DIR/gh_2.31.0_linux_amd64.deb"
 fi
 
 run_command "configuring github cli" \
