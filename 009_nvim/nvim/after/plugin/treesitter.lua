1098b50f1fc8b3a3e5d4cc23c2bd100ea2e7fae7
dcrblack <daniel.black@slicelife.com>
2023-08-17 12:14:01 +0100

    Disable treesitter highlighting for yaml files

--- 009_nvim/nvim/after/plugin/treesitter.lua
+++ 009_nvim/nvim/after/plugin/treesitter.lua
@@ -12,8 +12,13 @@ require'nvim-treesitter.configs'.setup {
 	highlight = {
 		enable = true,
 
-        -- Disable highlighting for large files
         disable = function(lang, buf)
+            -- Disable highlighting for yaml files
+            if lang == "yaml" then
+                return true
+            end
+
+            -- Disable highlighting for large files
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
