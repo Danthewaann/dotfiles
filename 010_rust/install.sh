c14a11b357702af2c48876d946921952bdd6dc69
dcrblack <daniel.black@slicelife.com>
2023-08-24 14:41:41 +0100

    Remove rust-analyzer install step as it's included by default now

--- 010_rust/install.sh
+++ 010_rust/install.sh
@@ -9,13 +9,3 @@ source "$SCRIPT_DIR"/../common
 
 run_command "installing rust" \
     "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
-
-run_command "installing rust-analyzer" \
-    "$(which rustup) component add rust-analyzer"
-
-if [[ -L "$HOME/.cargo/bin/rust-analyzer" ]]; then
-    rm -f "$HOME/.cargo/bin/rust-analyzer"
-fi
-
-info "linking rust-analyzer"
-link_file "$(rustup which --toolchain stable rust-analyzer)" "$HOME/.cargo/bin/rust-analyzer"
