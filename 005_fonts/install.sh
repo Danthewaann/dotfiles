667e7e044e85e29f2866f64cd75c3fd8daf98edb
dcrblack <danielcrblack@gmail.com>
2023-08-25 20:44:49 +0100

    Don't install fonts inside WSL

--- 005_fonts/install.sh
+++ 005_fonts/install.sh
@@ -7,6 +7,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck source=/dev/null
 source "$SCRIPT_DIR"/../common
 
+if inside_wsl; then
+    warn "skipping fonts setup inside WSL"
+    exit 0
+fi
+
 if [[ ! -f "$SCRIPT_DIR/Hack.zip" ]]; then
     run_command "downloading nerd hack fonts" \
         "wget -O $SCRIPT_DIR/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip"
