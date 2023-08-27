f0522f74f12fc20b4dec8d1eae02d4983e6860c9
dcrblack <danielcrblack@gmail.com>
2023-08-27 23:26:26 +0100

    Don't show modified icon in bufferline

--- 009_nvim/nvim/after/plugin/bufferline.lua
+++ 009_nvim/nvim/after/plugin/bufferline.lua
@@ -301,7 +301,7 @@ bufferline.setup({
             style = 'none',
         },
         buffer_close_icon = '',
-        modified_icon = '●',
+        modified_icon = '',
         close_icon = '',
         left_trunc_marker = '',
         right_trunc_marker = '',
