edf6583be924bbca7970abf83dd8eb77a37a5ca1
dcrblack <danielcrblack@gmail.com>
2023-08-27 23:26:10 +0100

    Add list to lualine winbar ignore

--- 009_nvim/nvim/after/plugin/lualine.lua
+++ 009_nvim/nvim/after/plugin/lualine.lua
@@ -52,7 +52,7 @@ require('lualine').setup({
         ignore_focus = { "NvimTree", "dbui", "undotree", "TelescopePrompt"},
         globalstatus = true,
         disabled_filetypes = {
-            winbar = { "qf", "fugitive", "dbui", "NvimTree", "undotree", "gitcommit", "GV", "packer", "", }
+            winbar = { "qf", "fugitive", "dbui", "NvimTree", "undotree", "gitcommit", "GV", "packer", "list", "" }
         },
     },
     extensions = { 'fugitive', 'nvim-tree', 'quickfix' },
