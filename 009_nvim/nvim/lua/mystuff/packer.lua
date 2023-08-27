58217905103ec3eaa1906926a8b0fd3d66ea548f
dcrblack <danielcrblack@gmail.com>
2023-08-27 14:16:48 +0100

    Add gitsigns plugin

--- 009_nvim/nvim/lua/mystuff/packer.lua
+++ 009_nvim/nvim/lua/mystuff/packer.lua
@@ -61,6 +61,7 @@ return require('packer').startup(function(use)
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 
+    use 'lewis6991/gitsigns.nvim'
     use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
 
     use 'nvim-pack/nvim-spectre'
