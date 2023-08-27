63f6cb72be026e69b9be4063083332574f769dcc
dcrblack <danielcrblack@gmail.com>
2023-08-27 16:44:52 +0100

    Colour scheme changes

--- 009_nvim/nvim/after/plugin/colourschema.lua
+++ 009_nvim/nvim/after/plugin/colourschema.lua
@@ -1,7 +1,9 @@
-vim.cmd[[colorscheme onedark]]
+require('onedark').setup({
+    style = 'dark'
+})
+
+require('onedark').load()
 
 vim.cmd("highlight NvimTreeNormal guibg=#282c34 guifg=#9da5b3")
 vim.cmd("highlight NvimTreeNormalFloat guibg=#282c34 guifg=#9da5b3")
 vim.cmd("highlight NvimTreeEndOfBuffer guibg=#282c34 guifg=#282c34")
-
-require("nvim-tree").setup({})
