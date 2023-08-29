84b5b557e66aa279e002166879b7e931c0fe3b73
dcrblack <daniel.black@slicelife.com>
2023-08-29 08:25:37 +0100

    Moved misc installs from vim to misc install script

---
+++ 001_misc/install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# shellcheck source=/dev/null
+source "$SCRIPT_DIR"/../common
+
+if [[ $OSTYPE == "darwin"* ]]; then
+    run_command "installing fzf" \
+        "brew install fzf"
+
+    run_command "installing shellcheck" \
+        "brew install shellcheck"
+
+    run_command "installing git-delta" \
+        "brew install git-delta"
+
+    run_command "installing bat" \
+        "brew install bat"
+
+    run_command "installing ripgrep" \
+        "brew install ripgrep"
+
+    run_command "installing watchman" \
+        "brew install watchman"
+else
+    run_command "installing fzf" \
+        "sudo apt-get install -y fzf"
+
+    run_command "installing shellcheck" \
+        "sudo apt-get install -y shellcheck"
+
+    if [[ ! -f "$SCRIPT_DIR/git-delta.deb" ]]; then
+        run_command "downloading git-delta" \
+            "wget -O $SCRIPT_DIR/git-delta.deb https://github.com/dandavison/delta/releases/download/0.15.1/git-delta_0.15.1_amd64.deb"
+    fi
+
+    run_command "installing git-delta" \
+        "sudo dpkg -i $SCRIPT_DIR/git-delta.deb"
+
+    if [[ ! -f "$SCRIPT_DIR/bat.deb" ]]; then
+        run_command "downloading bat" \
+            "wget -O $SCRIPT_DIR/bat.deb https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-musl_0.22.1_amd64.deb"
+    fi
+
+    run_command "installing bat" \
+        "sudo dpkg -i $SCRIPT_DIR/bat.deb"
+
+    run_command "installing ripgrep" \
+        "sudo apt-get install -y ripgrep"
+
+    run_command "installing watchman" \
+        "sudo apt-get install -y watchman"
+fi
--- 009_vim/install.sh
+++ 009_vim/install.sh
@@ -24,55 +24,6 @@ if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 fi
 
-# Install vim plugin dependencies
-if [[ $OSTYPE == "darwin"* ]]; then
-    run_command "installing fzf" \
-        "brew install fzf"
-
-    run_command "installing shellcheck" \
-        "brew install shellcheck"
-
-    run_command "installing git-delta" \
-        "brew install git-delta"
-
-    run_command "installing bat" \
-        "brew install bat"
-
-    run_command "installing ripgrep" \
-        "brew install ripgrep"
-
-    run_command "installing watchman" \
-        "brew install watchman"
-else
-    run_command "installing fzf" \
-        "sudo apt-get install -y fzf"
-
-    run_command "installing shellcheck" \
-        "sudo apt-get install -y shellcheck"
-
-    if [[ ! -f "$SCRIPT_DIR/git-delta.deb" ]]; then
-        run_command "downloading git-delta" \
-            "wget -O $SCRIPT_DIR/git-delta.deb https://github.com/dandavison/delta/releases/download/0.15.1/git-delta_0.15.1_amd64.deb"
-    fi
-
-    run_command "installing git-delta" \
-        "sudo dpkg -i $SCRIPT_DIR/git-delta.deb"
-
-    if [[ ! -f "$SCRIPT_DIR/bat.deb" ]]; then
-        run_command "downloading bat" \
-            "wget -O $SCRIPT_DIR/bat.deb https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-musl_0.22.1_amd64.deb"
-    fi
-
-    run_command "installing bat" \
-        "sudo dpkg -i $SCRIPT_DIR/bat.deb"
-
-    run_command "installing ripgrep" \
-        "sudo apt-get install -y ripgrep"
-
-    run_command "installing watchman" \
-        "sudo apt-get install -y watchman"
-fi
-
 # Install vim plugins
 run_command "installing vim plugins" \
     "vim --not-a-term +PlugInstall\! +qall > /dev/null 2>&1"
