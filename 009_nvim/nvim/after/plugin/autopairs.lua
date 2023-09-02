f3f45c60d011ad90f95da90d2649e5528c5de253
dcrblack <danielcrblack@gmail.com>
2023-09-02 17:11:48 +0100

    Make autopairs work with coc autocomplete

--- 009_nvim/nvim/after/plugin/autopairs.lua
+++ 009_nvim/nvim/after/plugin/autopairs.lua
@@ -1,1 +1,16 @@
-require("nvim-autopairs").setup({})
+local npairs = require("nvim-autopairs")
+npairs.setup({ map_cp = false })
+
+_G.MUtils= {}
+
+-- Make <CR> confirm an autocomplete selection
+-- From: https://github.com/windwp/nvim-autopairs/wiki/Completion-plugin
+MUtils.completion_confirm=function()
+    if vim.fn["coc#pum#visible"]() ~= 0  then
+        return vim.fn["coc#pum#confirm"]()
+    else
+        return npairs.autopairs_cr()
+    end
+end
+
+vim.keymap.set("i", "<CR>", "v:lua.MUtils.completion_confirm()", {expr = true , noremap = true})
