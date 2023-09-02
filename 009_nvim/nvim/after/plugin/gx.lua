4ad673f7ceeb21fd7570ee67c25269947647e305
dcrblack <danielcrblack@gmail.com>
2023-09-02 20:55:45 +0100

    Fix gx.nvim to work in WSL

--- 009_nvim/nvim/after/plugin/gx.lua
+++ 009_nvim/nvim/after/plugin/gx.lua
@@ -1,6 +1,24 @@
+function file_exists(name)
+   local f = io.open(name, "r")
+   return f ~= nil and io.close(f)
+end
+
+local browser_app = ""
+
+if (file_exists("/proc/sys/fs/binfmt_misc/WSLInterop")) then
+    browser_app = "powershell.exe"
+    browser_args = { "start explorer.exe" }
+elseif (jit.os == "Darwin") then
+    browser_app = "open"
+    browser_args = { "--background" }
+else
+    browser_app = "xdg-open"
+    browser_args = {}
+end
+
 require("gx").setup({
-    open_browser_app = "open", -- specify your browser app; default for macOS is "open", Linux "xdg-open" and Windows "powershell.exe"
-    open_browser_args = { "--background" }, -- specify any arguments, such as --background for macOS' "open".
+    open_browser_app = browser_app, -- specify your browser app; default for macOS is "open", Linux "xdg-open" and Windows "powershell.exe"
+    open_browser_args = browser_args, -- specify any arguments, such as --background for macOS' "open".
     handlers = {
         plugin = true, -- open plugin links in lua (e.g. packer, lazy, ..)
         github = true, -- open github issues
