2dcfc0c9799f61281b039c29c9055eb54cac3125
dcrblack <daniel.black@slicelife.com>
2023-10-12 10:40:07 +0100

    Remove <leader>sf bind for spectre

--- 009_nvim/nvim/after/plugin/nvim-spectre.lua
+++ 009_nvim/nvim/after/plugin/nvim-spectre.lua
@@ -13,7 +13,3 @@ end, {
 vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
     desc = "Search current word"
 })
-
-vim.keymap.set('n', '<leader>sf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-    desc = "Search on current file"
-})
