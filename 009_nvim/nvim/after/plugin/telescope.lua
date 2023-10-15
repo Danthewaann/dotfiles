2ae0739533a6d817f800fef82ffa870cb030020e
dcrblack <danielcrblack@gmail.com>
2023-10-15 18:12:40 +0100

    Adjust telescope layouts for specific pickers

--- 009_nvim/nvim/after/plugin/telescope.lua
+++ 009_nvim/nvim/after/plugin/telescope.lua
@@ -20,8 +20,8 @@ telescope.setup {
         file_ignore_patterns = {
             "vendor"
         },
-        layout_strategy = 'vertical',
-        layout_config = { height = 0.99 },
+        layout_strategy = 'horizontal',
+        layout_config = { horizontal = { height = 0.9, width = 0.9, preview_width = 0.55 }},
     },
     pickers = {
         live_grep = {
@@ -46,13 +46,19 @@ function vim.getVisualSelection()
     end
 end
 
+local function mergeTables(firstTable, secondTable)
+    for k,v in pairs(secondTable) do firstTable[k] = v end
+    return firstTable
+end
+
 local builtin = require('telescope.builtin')
+local vertical_layout = { layout_strategy='vertical', layout_config={ height = 0.9, width = 0.9, preview_height = 0.6  } }
 
 -- Resume last picker
 vim.keymap.set('n', '<leader>tr', builtin.resume, {})
 
 -- List previous pickers
-vim.keymap.set('n', '<leader>tp', builtin.pickers, {})
+vim.keymap.set('n', '<leader>tp', function() builtin.pickers(vertical_layout) end)
 
 -- Search through all files in the current project
 vim.keymap.set('n', '<C-p>', function()
@@ -63,33 +69,36 @@ end)
 vim.keymap.set('n', '<C-f>', builtin.git_files, {})
 
 -- List all open and hidden buffers
-vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>bb', function() builtin.buffers(vertical_layout) end)
 
 -- List search history
-vim.keymap.set('n', '<leader>hs', builtin.search_history, {})
+vim.keymap.set('n', '<leader>sh', builtin.search_history, {})
 
 -- List command history
-vim.keymap.set('n', '<leader>hc', builtin.command_history, {})
+vim.keymap.set('n', '<leader>ch', builtin.command_history, {})
 
 -- Show changed git files in current project
-vim.keymap.set('n', '<leader>gf', builtin.git_status, {})
+vim.keymap.set('n', '<leader>gf', function() builtin.git_status(vertical_layout) end)
 
 -- Show all git commits in current project
-vim.keymap.set('n', '<leader>gl', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gl', function() builtin.git_commits(vertical_layout) end)
 
 -- Search for pattern in current project files
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    local ok, search = pcall(vim.fn.input, "Grep > ")
+    if ok then
+        builtin.grep_string(mergeTables({ search = test }, vertical_layout))
+    end
 end)
 
 -- Search for the current word in project files
-vim.keymap.set('n', '<leader>F', builtin.grep_string, {});
+vim.keymap.set('n', '<leader>F', function() builtin.grep_string(vertical_layout) end);
 
 -- Search for the current visual selection in project files
 vim.keymap.set('v', '<leader>F', function()
     local test = vim.getVisualSelection()
-    builtin.grep_string({ search = test })
+    builtin.grep_string(mergeTables({ search = test }, vertical_layout))
 end);
 
 -- Do a live grep in the current project
-vim.keymap.set('n', '<leader>lg', builtin.live_grep, {});
+vim.keymap.set('n', '<leader>lg', function() builtin.live_grep(vertical_layout) end);
