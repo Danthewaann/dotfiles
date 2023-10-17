9cb9d154445bae3b510e6f84de42475c16ba9add
dcrblack <daniel.black@slicelife.com>
2023-10-17 17:42:23 +0100

    LSP jump fold changes

--- 009_nvim/nvim/init.lua
+++ 009_nvim/nvim/init.lua
@@ -9,7 +9,7 @@ local function center_and_unfold(func, opts)
     opts = opts or {}
     func(opts)
     vim.defer_fn(function()
-      pcall(vim.cmd, ":normal! zzzv")
+      pcall(vim.cmd, ":normal! zzzvzczO")
     end, 100)
   end
 
@@ -364,8 +364,8 @@ vim.defer_fn(function()
 end, 0)
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', center_and_unfold(vim.diagnostic.goto_prev), { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', center_and_unfold(vim.diagnostic.goto_next), { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
