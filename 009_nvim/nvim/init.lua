69be6cfbd8d828fb1454fe6cc8e055003d553e3e
dcrblack <daniel.black@slicelife.com>
2023-10-17 16:12:34 +0100

    Rename :Format to :W

--- 009_nvim/nvim/init.lua
+++ 009_nvim/nvim/init.lua
@@ -393,8 +393,8 @@ local on_attach = function(_, bufnr)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
 
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  -- Create a command `:W` local to the LSP buffer to format the code
+  vim.api.nvim_buf_create_user_command(bufnr, 'W', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
 end
