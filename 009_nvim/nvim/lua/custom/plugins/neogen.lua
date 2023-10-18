e63361a93d1297c741d5da4fd149abf53c4457a0
dcrblack <daniel.black@slicelife.com>
2023-10-18 12:22:49 +0100

    Use <leader>gd to generate doc strings in neogen

--- 009_nvim/nvim/lua/custom/plugins/neogen.lua
+++ 009_nvim/nvim/lua/custom/plugins/neogen.lua
@@ -1,14 +1,15 @@
 return {
     "danymat/neogen",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    config = function ()
+    config = function()
         require('neogen').setup {
             enabled = true,             --if you want to disable Neogen
             input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
             -- jump_map = "<C-e>"       -- (DROPPED SUPPORT, see [here](#cycle-between-annotations) !) The keymap in order to jump in the annotation fields (in insert mode)
         }
 
-        local opts = { noremap = true, silent = true }
-        vim.keymap.set("n", "<leader>ds", ":lua require('neogen').generate()<CR>", opts)
+        vim.keymap.set("n", "<leader>gd", ":lua require('neogen').generate()<CR>",
+            { noremap = true, silent = true, desc = '[G]enerate [D]ocstring' }
+        )
     end
 }
