f65f0b48969e01547481fcb1c871ad748487b7aa
dcrblack <daniel.black@slicelife.com>
2023-10-20 13:56:58 +0100

    Adjust telescope buffer list binds

--- 009_nvim/nvim/init.lua
+++ 009_nvim/nvim/init.lua
@@ -284,17 +284,16 @@ end
 pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles,
-  {
-    desc
-    = '[?] Find recently opened files'
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', function()
+  require('telescope.builtin').buffers(require('telescope.themes').get_dropdown {
+    previewer = false,
+    sort_mru = true,
   })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers,
-  { desc = '[ ] Find existing buffers' })
+end, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
