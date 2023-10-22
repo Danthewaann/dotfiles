3503baba686e731322841f84e73356020211e04d
dcrblack <daniel.black@slicelife.com>
2023-10-22 12:25:09 +0100

    Move gitsigns config into separate file

---
+++ 009_nvim/nvim/lua/custom/plugins/gitsigns.lua
@@ -0,0 +1,60 @@
+return {
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    -- See `:help gitsigns.txt`
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      map("n", "<leader>hs", gs.stage_hunk, { buffer = bufnr, desc = "Stage git hunk" })
+      map("n", "<leader>hr", gs.reset_hunk, { buffer = bufnr, desc = "Reset git hunk" })
+      map("v", "<leader>hs", function()
+        gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, { buffer = bufnr, desc = "Stage git hunk" })
+      map("v", "<leader>hr", function()
+        gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, { buffer = bufnr, desc = "Resert git hunk" })
+      map("n", "<leader>hS", gs.stage_buffer, { buffer = bufnr, desc = "Stage buffer" })
+      map("n", "<leader>hu", gs.undo_stage_hunk, { buffer = bufnr, desc = "Undo staged hunk" })
+      map("n", "<leader>hR", gs.reset_buffer, { buffer = bufnr, desc = "Reset buffer" })
+      map("n", "<leader>hp", gs.preview_hunk, { buffer = bufnr, desc = "Preview git hunk" })
+      map("n", "<leader>hb", function()
+        gs.blame_line({ full = true })
+      end, { buffer = bufnr, desc = "Git blame current line" })
+      map("n", "<leader>hd", gs.diffthis, { buffer = bufnr, desc = "Show diff of buffer" })
+      map("n", "<leader>hD", function()
+        gs.diffthis("~")
+      end, { buffer = bufnr, desc = "Show diff of buffer" })
+      map("n", "<leader>td", gs.toggle_deleted, { buffer = bufnr, desc = "Toggle deleted lines" })
+
+      -- Text object
+      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { buffer = bufnr, desc = "Select inner hunk" })
+
+      -- don't override the built-in and fugitive keymaps
+      map({ "n", "v" }, "]c", function()
+        if vim.wo.diff then
+          return "]c"
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+        return "<Ignore>"
+      end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
+      map({ "n", "v" }, "[c", function()
+        if vim.wo.diff then
+          return "[c"
+        end
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
+        return "<Ignore>"
+      end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
+    end,
+  },
+}
