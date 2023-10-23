3924e7e2952ec28536f1f4e3edeca6cff8296910
dcrblack <daniel.black@slicelife.com>
2023-10-23 14:26:25 +0100

    Adjust aerial config and binds

--- 009_nvim/nvim/lua/custom/plugins/aerial.lua
+++ 009_nvim/nvim/lua/custom/plugins/aerial.lua
@@ -17,7 +17,7 @@ return {
         -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
         -- min_width and max_width can be a list of mixed types.
         -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
-        max_width = { 40, 0.3 },
+        max_width = { 0.5 },
         width = nil,
         min_width = 10,
 
@@ -61,6 +61,7 @@ return {
       keymaps = {
         ["?"] = "actions.show_help",
         ["g?"] = "actions.show_help",
+        ["<Tab>"] = "actions.jump",
         ["<CR>"] = "actions.jump",
         ["<2-LeftMouse>"] = "actions.jump",
         ["<C-v>"] = "actions.jump_vsplit",
@@ -361,4 +362,6 @@ return {
       },
     })
   end,
+
+  vim.keymap.set("n", "<leader>ds", "<cmd>AerialToggle<CR>",  { desc = "[D]ocument [S]symbols" })
 }
