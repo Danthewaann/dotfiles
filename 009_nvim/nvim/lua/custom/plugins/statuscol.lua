0edec0772418738da022ce2a1c0f127e66e520e7
dcrblack <daniel.black@slicelife.com>
2023-10-25 20:29:36 +0100

    Add statuscol.nvim plugin

---
+++ 009_nvim/nvim/lua/custom/plugins/statuscol.lua
@@ -0,0 +1,24 @@
+return {
+  "luukvbaal/statuscol.nvim",
+  config = function()
+    local builtin = require("statuscol.builtin")
+    require("statuscol").setup({
+      relculright = true,
+      segments = {
+        {
+          sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true },
+          click = "v:lua.ScSa"
+        },
+        {
+          sign = { text = { ".*" } },
+          click = "v:lua.ScSa"
+        },
+        {
+          text = { builtin.lnumfunc, " " },
+          condition = { true, builtin.not_empty },
+          click = "v:lua.ScLa",
+        },
+      }
+    })
+  end,
+}
