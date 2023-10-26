4a7f160e1404ea9a62226244ef379d38f1b56e34
dcrblack <daniel.black@slicelife.com>
2023-10-26 10:51:06 +0100

    Ignore terminal buffers in statuscol.nvim plugin

--- 009_nvim/nvim/lua/custom/plugins/statuscol.lua
+++ 009_nvim/nvim/lua/custom/plugins/statuscol.lua
@@ -4,21 +4,22 @@ return {
     local builtin = require("statuscol.builtin")
     require("statuscol").setup({
       relculright = true,
+      bt_ignore = { "terminal" },
       segments = {
         {
           sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true },
-          click = "v:lua.ScSa"
+          click = "v:lua.ScSa",
         },
         {
           sign = { text = { ".*" } },
-          click = "v:lua.ScSa"
+          click = "v:lua.ScSa",
         },
         {
           text = { builtin.lnumfunc, " " },
           condition = { true, builtin.not_empty },
           click = "v:lua.ScLa",
         },
-      }
+      },
     })
   end,
 }
