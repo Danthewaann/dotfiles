b6c8066a0adb77b5f806fed5d4b768e27c0c229e
dcrblack <daniel.black@slicelife.com>
2023-10-27 13:25:26 +0100

    Use mypy in virtual env in nvim-lint plugin

--- 009_nvim/nvim/lua/custom/plugins/nvim-lint.lua
+++ 009_nvim/nvim/lua/custom/plugins/nvim-lint.lua
@@ -6,6 +6,19 @@ return {
       go = { "golangcilint" },
     }
 
+    local mypy_linter = require("lint").linters.mypy
+
+    -- Use mypy in virtual environment if found
+    ---@diagnostic disable-next-line: assign-type-mismatch
+    mypy_linter.cmd = function()
+      local local_mypy = vim.fn.fnamemodify(".venv/bin/mypy", ":p")
+      local stat = vim.loop.fs_stat(local_mypy)
+      if stat then
+        return local_mypy
+      end
+      return "mypy"
+    end
+
     vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost", "InsertLeave" }, {
       callback = function()
         require("lint").try_lint()
