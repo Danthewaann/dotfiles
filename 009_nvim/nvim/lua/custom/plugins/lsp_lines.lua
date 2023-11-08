6736a7651d631b46c1031e5bf667d188ed2954c2
dcrblack <daniel.black@slicelife.com>
2023-11-08 09:42:38 +0000

    Adjust vim.diagnostic.config code in lsp_lines plugin

--- 009_nvim/nvim/lua/custom/plugins/lsp_lines.lua
+++ 009_nvim/nvim/lua/custom/plugins/lsp_lines.lua
@@ -5,29 +5,34 @@ return {
 
     -- Enable virtual text by default, but add a toggle to switch
     -- between virtual text and virtual lines
-    local virtual_text = true
-    local virtual_lines_config = { only_current_line = false, highlight_whole_line = true }
-    local config = { virtual_text = virtual_text, virtual_lines = virtual_lines_config }
+    local use_virtual_text = true
+    local virtual_text_config = { source = "if_many" }
+    local virtual_lines_config = {
+      only_current_line = false,
+      highlight_whole_line = true,
+    }
+    local diagnostics_config = {
+      virtual_text = virtual_text_config,
+      signs = { enable = true },
+      float = { source = "always" },
+      virtual_lines = false,
+    }
 
     -- Setup initial diagnostic config
-    vim.diagnostic.config({
-      virtual_text = virtual_text,
-      signs = { enable = true },
-      virtual_lines = false,
-    })
+    vim.diagnostic.config(diagnostics_config)
 
     vim.keymap.set("", "<leader>l", function()
-      virtual_text = not virtual_text
-      if virtual_text then
+      use_virtual_text = not use_virtual_text
+      if use_virtual_text then
         print("Toggling virtual text")
-        config.virtual_text = true
-        config.virtual_lines = false
+        diagnostics_config.virtual_text = virtual_text_config
+        diagnostics_config.virtual_lines = false
       else
         print("Toggling virtual lines")
-        config.virtual_text = false
-        config.virtual_lines = virtual_lines_config
+        diagnostics_config.virtual_text = false
+        diagnostics_config.virtual_lines = virtual_lines_config
       end
-      vim.diagnostic.config(config)
+      vim.diagnostic.config(diagnostics_config)
     end, { desc = "Toggle between LSP virtual lines and virtual text" })
   end,
 }
