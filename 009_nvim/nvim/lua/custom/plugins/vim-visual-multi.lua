22e87902cf03e2a37fef1b8cf37d2e653010c010
dcrblack <daniel.black@slicelife.com>
2023-11-08 08:30:22 +0000

    Properly setup vim-visual-multi.vim plugin

--- 009_nvim/nvim/lua/custom/plugins/vim-visual-multi.lua
+++ 009_nvim/nvim/lua/custom/plugins/vim-visual-multi.lua
@@ -1,15 +1,20 @@
 return {
   "mg979/vim-visual-multi",
   init = function()
-    vim.g.VM_set_statusline = 0 -- disable VM's statusline updates to prevent clobbering
-    vim.g.VM_silent_exit = 1    -- because the status line already tells me the mode
+    vim.g.VM_set_statusline   = 0 -- disable VM's statusline updates to prevent clobbering
+    vim.g.VM_silent_exit      = 1 -- because the status line already tells me the mode
+    vim.g.VM_default_mappings = 1
+    vim.g.VM_maps             = { ["Find Under"] = "" }
+    vim.g.VM_Mono_hl          = "DiffText"
+    vim.g.VM_Extend_hl        = "Visual"
+    vim.g.VM_Cursor_hl        = "Visual"
+    vim.g.VM_Insert_hl        = "DiffChange"
   end,
   config = function()
-    vim.cmd([[
-      let g:VM_Mono_hl   = 'DiffText'
-      let g:VM_Extend_hl = 'Visual'
-      let g:VM_Cursor_hl = 'Visual'
-      let g:VM_Insert_hl = 'DiffChange'
-    ]])
+    vim.keymap.set("n", "<leader>v/", "<Plug>(VM-Start-Regex-Search)", { desc = "[V]isual multi start regex search" })
+    vim.keymap.set("n", "<leader>vn", "<Plug>(VM-Find-Under)", { desc = "[V]isual multi find under cursor" })
+    vim.keymap.set("n", "<leader>va", "<Plug>(VM-Select-All)", { desc = "[V]isual multi select all under cursor" })
+    vim.keymap.set("n", "<leader>vj", "<Plug>(VM-Add-Cursor-Down)", { desc = "[V]isual multi add cursor down" })
+    vim.keymap.set("n", "<leader>vk", "<Plug>(VM-Add-Cursor-Up)", { desc = "[V]isual multi add cursor up" })
   end
 }
