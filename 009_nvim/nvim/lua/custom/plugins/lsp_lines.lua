9869b3dbdb524d7c26f8ee7f2311bc7f5ccb9104
dcrblack <daniel.black@slicelife.com>
2023-11-09 08:35:58 +0000

    Add bind to toggle virtual diagnostics on and off

--- 009_nvim/nvim/lua/custom/plugins/lsp_lines.lua
+++ 009_nvim/nvim/lua/custom/plugins/lsp_lines.lua
@@ -6,7 +6,8 @@ return {
     -- Enable virtual text by default, but add a toggle to switch
     -- between virtual text and virtual lines
     local use_virtual_text = true
-    local virtual_text_config = { source = "if_many" }
+    local virtual_text_or_lines_visible = true
+    local virtual_text_config = { source = "always" }
     local virtual_lines_config = {
       only_current_line = false,
       highlight_whole_line = true,
@@ -18,21 +19,49 @@ return {
       virtual_lines = false,
     }
 
-    -- Setup initial diagnostic config
-    vim.diagnostic.config(diagnostics_config)
-
-    vim.keymap.set("", "<leader>l", function()
-      use_virtual_text = not use_virtual_text
+    local function configure_virtual_text_and_lines(output)
+      output = output or false
       if use_virtual_text then
-        print("Toggling virtual text")
+        if output then
+          print("Toggling virtual text")
+        end
         diagnostics_config.virtual_text = virtual_text_config
         diagnostics_config.virtual_lines = false
       else
-        print("Toggling virtual lines")
+        if output then
+          print("Toggling virtual lines")
+        end
         diagnostics_config.virtual_text = false
         diagnostics_config.virtual_lines = virtual_lines_config
       end
+    end
+
+    -- Setup initial diagnostic config
+    vim.diagnostic.config(diagnostics_config)
+
+    vim.keymap.set("n", "<leader>dt", function()
+      use_virtual_text = not use_virtual_text
+
+      configure_virtual_text_and_lines(true)
+
+      if virtual_text_or_lines_visible then
+        vim.diagnostic.config(diagnostics_config)
+      end
+    end, { desc = "[D]iagnostic [T]oggle between virtual text and lines" })
+
+    vim.keymap.set("n", "<leader>do", function()
+      virtual_text_or_lines_visible = not virtual_text_or_lines_visible
+
+      if virtual_text_or_lines_visible then
+        print("Toggling on virtual diagnostics")
+        configure_virtual_text_and_lines()
+      else
+        print("Toggling off virtual diagnostics")
+        diagnostics_config.virtual_text = false
+        diagnostics_config.virtual_lines = false
+      end
+
       vim.diagnostic.config(diagnostics_config)
-    end, { desc = "Toggle between LSP virtual lines and virtual text" })
+    end, { desc = "[D]iagnostic toggle [O]n or [O]ff virtual diagnostics" })
   end,
 }
