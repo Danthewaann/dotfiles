2229535c5b70c9103c71df0a9753e61208feeb99
Danthewaann <daniel.black@slicelife.com>
2023-11-16 11:38:54 +0000

    Update symbol style in symbol-usage.lua plugin

--- 009_nvim/nvim/lua/plugins/symbol-usage.lua
+++ 009_nvim/nvim/lua/plugins/symbol-usage.lua
@@ -2,24 +2,40 @@ return {
   "Wansmer/symbol-usage.nvim",
   event = "LspAttach", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
   config = function()
+    local function h(name) return vim.api.nvim_get_hl(0, { name = name }) end
+
+    vim.api.nvim_set_hl(0, "SymbolUsageContent", { fg = h("Comment").fg, italic = true })
+    vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("Function").fg, italic = true })
+    vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("Type").fg, italic = true })
+    vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, italic = true })
+
     local function text_format(symbol)
       local fragments = {}
 
-      if symbol.references then
+      if symbol.references > 0 then
         local usage = symbol.references <= 1 and "usage" or "usages"
-        local num = symbol.references == 0 and "no" or symbol.references
-        table.insert(fragments, ("%s %s"):format(num, usage))
+        local num = symbol.references
+        table.insert(fragments, { "󰌹 ", "SymbolUsageRef" })
+        table.insert(fragments, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
       end
 
       if symbol.definition then
-        table.insert(fragments, symbol.definition .. " defs")
+        if #fragments > 0 then
+          table.insert(fragments, { " ", "NonText" })
+        end
+        table.insert(fragments, { "󰳽 ", "SymbolUsageDef" })
+        table.insert(fragments, { symbol.definition .. " defs", "SymbolUsageContent" })
       end
 
       if symbol.implementation then
-        table.insert(fragments, symbol.implementation .. " impls")
+        if #fragments > 0 then
+          table.insert(fragments, { " ", "NonText" })
+        end
+        table.insert(fragments, { "󰡱 ", "SymbolUsageImpl" })
+        table.insert(fragments, { symbol.implementation .. " impls", "SymbolUsageContent" })
       end
 
-      return "-> " .. table.concat(fragments, ", ")
+      return fragments
     end
 
     local SymbolKind = vim.lsp.protocol.SymbolKind
