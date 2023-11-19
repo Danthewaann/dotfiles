a9847a310c9299707876571e21b29e10be53e2fd
Danthewaann <daniel.black@slicelife.com>
2023-11-19 15:59:08 +0000

    Adjust telescope pickers layout config

--- 009_nvim/nvim/lua/plugins/telescope.lua
+++ 009_nvim/nvim/lua/plugins/telescope.lua
@@ -18,10 +18,12 @@ return {
     "nvim-telescope/telescope-file-browser.nvim",
     "piersolenski/telescope-import.nvim"
   },
-  config = function ()
+  config = function()
+    local actions = require("telescope.actions")
     require("telescope").setup({
       defaults = {
         -- Cache the last 10 pickers so I can resume them later
+        wrap_results = true,
         cache_picker = {
           num_pickers = 10,
           limit_entries = 1000,
@@ -37,7 +39,14 @@ return {
           "--hidden",
         },
         file_ignore_patterns = {
-          "vendor",
+          "^vendor",
+          "^.git",
+          "^node_modules",
+          "^.venv",
+          "^.mypy_cache",
+          "^.cache",
+          "__pycache__",
+          "^.coverage",
         },
         layout_strategy = "vertical",
         layout_config = {
@@ -46,20 +55,131 @@ return {
         },
       },
       pickers = {
+        oldfiles = {
+          theme = "dropdown",
+          previewer = false,
+          layout_config = {
+            height = 0.6
+          },
+        },
+        find_files = {
+          theme = "dropdown",
+          previewer = false,
+          layout_config = {
+            height = 0.6
+          },
+        },
+        help_tags = {
+          theme = "dropdown",
+          previewer = true,
+          layout_config = {
+            height = 0.5,
+            width = 0.5,
+          },
+        },
+        git_commits = {
+          theme = "dropdown",
+          previewer = true,
+          layout_config = {
+            height = 0.4,
+          },
+        },
+        git_status = {
+          theme = "dropdown",
+          previewer = true,
+          layout_config = {
+            height = 0.4,
+          },
+        },
+        pickers = {
+          theme = "dropdown",
+          previewer = false,
+        },
+        man_pages = {
+          theme = "dropdown",
+          previewer = false,
+          layout_config = {
+            height = 0.5,
+            width = 0.5,
+          },
+        },
+        diagnostics = {
+          theme = "dropdown",
+          previewer = true,
+          layout_config = {
+            height = 0.5,
+            width = 0.5,
+          },
+        },
+        grep_string = {
+          theme = "dropdown",
+          preview = true,
+          layout_config = {
+            height = 0.5,
+            width = 0.5,
+          },
+          additional_args = function(_)
+            return { "--hidden" }
+          end,
+        },
         live_grep = {
+          theme = "dropdown",
+          previewer = true,
+          layout_config = {
+            height = 0.5,
+            width = 0.5,
+          },
           additional_args = function(_)
             return { "--hidden" }
           end,
         },
       },
       extensions = {
+        import = {
+          layout_config = {
+            prompt_position = "top",
+            height = 0.6,
+            width = 0.4,
+          },
+        },
         file_browser = {
-          layout_strategy = "horizontal",
+          theme = "dropdown",
+          layout_config = {
+            height = 0.6
+          },
           hijack_netrw = true,
+          respect_gitignore = false,
           hidden = true,
           grouped = true,
-          previewer = true,
-          initial_mode = "normal",
+          previewer = false,
+          mappings = {
+            ["n"] = {
+              ["e"] = false,
+              ["<C-U>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_previous(prompt_bufnr)
+                end
+              end,
+              ["<C-D>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_next(prompt_bufnr)
+                end
+              end,
+            },
+            ["i"] = {
+              ["<C-e>"] = false,
+              ["<C-u>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_previous(prompt_bufnr)
+                end
+              end,
+              ["<C-d>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_next(prompt_bufnr)
+                end
+              end,
+            }
+          }
         },
       }
     })
@@ -86,30 +206,19 @@ return {
         previewer = false,
       }))
     end, { desc = "[/] Fuzzily search in current buffer" })
-    vim.keymap.set("n", "<C-f>", function()
-      require("telescope.builtin").git_files({ layout_strategy = "horizontal" })
-    end, { desc = "Search Git Files" })
     vim.keymap.set("n", "<C-p>", function()
-      require("telescope.builtin").find_files({ layout_strategy = "horizontal", hidden = true })
+      require("telescope.builtin").find_files({ hidden = true, no_ignore = false })
     end, { desc = "Search Files" })
-    vim.keymap.set("n", "<leader>sh", function()
-      require("telescope.builtin").help_tags({ layout_strategy = "horizontal" })
-    end, { desc = "[S]earch [H]elp" })
-    vim.keymap.set("n", "<leader>sm", function()
-      require("telescope.builtin").man_pages({ layout_strategy = "horizontal" })
-    end, { desc = "[S]earch [M]an Pages" })
+    vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+    vim.keymap.set("n", "<leader>sm", require("telescope.builtin").man_pages, { desc = "[S]earch [M]an Pages" })
     vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
     vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
     vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
     vim.keymap.set("n", "<leader>sp", require("telescope.builtin").pickers, { desc = "[S]earch [P]ickers" })
-    vim.keymap.set("n", "<leader>gf", function()
-      require("telescope.builtin").git_status({ layout_strategy = "horizontal" })
-    end, { desc = "[G]it [F]iles" })
+    vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_status, { desc = "[G]it [F]iles" })
     vim.keymap.set("n", "<leader>gtl", require("telescope.builtin").git_commits, { desc = "[G]it [T]elescope [L]ogs" })
     vim.keymap.set("n", "<leader>ii", require("telescope").extensions.import.import, { desc = "[I]nsert [I]mport" })
-    vim.keymap.set("n", "<leader>fb", function()
-      require("telescope").extensions.file_browser.file_browser()
-    end)
+    vim.keymap.set("n", "<leader>fb", require("telescope").extensions.file_browser.file_browser)
     vim.keymap.set("n", "<leader>ff", function()
       require("telescope").extensions.file_browser.file_browser({
         path = "%:p:h",
