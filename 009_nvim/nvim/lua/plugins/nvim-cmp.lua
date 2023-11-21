03f154c7b8e2d57b8ad26b6122b2f407bbbffc13
Danthewaann <daniel.black@slicelife.com>
2023-11-21 10:29:30 +0000

    Make <C-e> start nvim-cmp completion

--- 009_nvim/nvim/lua/plugins/nvim-cmp.lua
+++ 009_nvim/nvim/lua/plugins/nvim-cmp.lua
@@ -59,7 +59,7 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete({}),
+        ["<C-e>"] = cmp.mapping.complete({}),
         ["<CR>"] = cmp.mapping.confirm({
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
