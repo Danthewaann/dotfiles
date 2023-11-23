87e7d326da7cf203aba65d63ca682ca04559574a
Danthewaann <daniel.black@slicelife.com>
2023-11-23 16:19:30 +0000

    Add table of error codes to ignore for mypy in nvim-lint

--- 009_nvim/nvim/lua/plugins/nvim-lint.lua
+++ 009_nvim/nvim/lua/plugins/nvim-lint.lua
@@ -13,10 +13,31 @@ return {
 
     -- Use mypy in virtual environment if found
     mypy_linter.cmd = utils.get_poetry_venv_executable_path("mypy")
+    mypy_linter.args = {
+      "--show-column-numbers",
+      "--show-error-end",
+      "--show-error-codes",
+      "--hide-error-context",
+      "--no-color-output",
+      "--no-error-summary",
+      "--no-pretty",
+    }
+
+    -- This is a table of error codes I ignore as I let pyright handle them instead
+    local error_codes = { "assignment" }
+    for _, code in ipairs(error_codes) do
+      table.insert(mypy_linter.args, "--disable-error-code")
+      table.insert(mypy_linter.args, code)
+    end
 
     -- From: https://github.com/conventional-changelog/commitlint/issues/613#issuecomment-1061807137
     local npm_root = vim.fn.trim(vim.fn.system("npm root -g"))
-    commit_linter.args = { "-g", os.getenv("HOME") .. "/.commitlintrc.yaml", "-x", npm_root .. "/@commitlint/config-conventional" }
+    commit_linter.args = {
+      "-g",
+      os.getenv("HOME") .. "/.commitlintrc.yaml",
+      "-x",
+      npm_root .. "/@commitlint/config-conventional",
+    }
 
     vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost", "InsertLeave" }, {
       callback = function()
