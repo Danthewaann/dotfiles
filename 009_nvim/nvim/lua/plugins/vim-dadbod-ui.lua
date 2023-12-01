123d86eebde55101ecd31d2c7b61745a6b69a0ac
Danthewaann <daniel.black@slicelife.com>
2023-12-01 18:56:01 +0000

    Use my fork of vim-dadbod-ui

--- 009_nvim/nvim/lua/plugins/vim-dadbod-ui.lua
+++ 009_nvim/nvim/lua/plugins/vim-dadbod-ui.lua
@@ -1,5 +1,5 @@
 return {
-  "kristijanhusak/vim-dadbod-ui",
+  "Danthewaann/vim-dadbod-ui",
   config = function()
     local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
     local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
