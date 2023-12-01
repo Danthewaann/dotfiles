b5d4d59310d636601d60a822fe0b40bdc9d1d689
Danthewaann <daniel.black@slicelife.com>
2023-12-01 18:14:00 +0000

    Update size to use a function in toggleterm.nvim plugin

--- 009_nvim/nvim/lua/plugins/toggleterm.lua
+++ 009_nvim/nvim/lua/plugins/toggleterm.lua
@@ -6,7 +6,11 @@ return {
       shade_terminals = false,
       direction = "vertical",
       size = function(term)
-        return vim.o.columns * 0.5
+        if term.direction == "horizontal" then
+          return 20
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.5
+        end
       end
     })
   end
