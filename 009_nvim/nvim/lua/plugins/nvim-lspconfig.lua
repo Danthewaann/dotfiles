85aa1c04a3584448be9fc362dfa6f3b780536e01
dcrblack <danielcrblack@gmail.com>
2023-12-05 22:30:01 +0000

    Adjust diagnostic bind descriptions

--- 009_nvim/nvim/lua/plugins/nvim-lspconfig.lua
+++ 009_nvim/nvim/lua/plugins/nvim-lspconfig.lua
@@ -56,8 +56,8 @@ return {
     -- Diagnostic keymaps
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating [D]iagnostic [M]essage" })
-    vim.keymap.set("n", "<leader>q", vim.diagnostic.setqflist, { desc = "Open [D]iagnostics [L]ist" })
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show floating diagnostic message" })
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setqflist, { desc = "Open diagnostics list" })
 
     -- [[ Configure LSP ]]
     -- This function gets run when an LSP connects to a particular buffer.
