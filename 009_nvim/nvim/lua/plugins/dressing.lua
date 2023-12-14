d4ce4269eaaf3dce22d9f90798df3944f41b2972
Danthewaann <danielcrblack@gmail.com>
2023-12-14 21:50:22 +0000

    Use cursor position for LSP rename popup

--- 009_nvim/nvim/lua/plugins/dressing.lua
+++ 009_nvim/nvim/lua/plugins/dressing.lua
@@ -5,6 +5,15 @@ return {
       insert_only = false,
       start_in_insert = true,
       relative = "editor",
+      get_config = function(opts)
+        if opts.prompt == "New Name: " then
+          return {
+            insert_only = true,
+            start_in_insert = true,
+            relative = "cursor"
+          }
+        end
+      end
     },
   },
 }
