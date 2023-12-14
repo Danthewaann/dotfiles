4f2e893b1fc87a15cc34964216bdaa9aaa210642
Danthewaann <danielcrblack@gmail.com>
2023-12-14 21:38:42 +0000

    Use rounded border for lsp_signature

--- 009_nvim/nvim/lua/plugins/nvim-lspconfig.lua
+++ 009_nvim/nvim/lua/plugins/nvim-lspconfig.lua
@@ -305,7 +305,7 @@ return {
     require("lsp_signature").setup({
       bind = true, -- This is mandatory, otherwise border config won't get registered.
       handler_opts = {
-        border = "none"
+        border = "rounded"
       },
       padding = "  ",
       fix_pos = true,
