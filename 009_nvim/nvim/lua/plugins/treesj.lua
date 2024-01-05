82accb81af26ddb872fbd8ce85c8e0b459e8ff92
Danthewaann <daniel.black@slicelife.com>
2024-01-05 10:23:11 +0000

    Adjust line split/join binds

--- 009_nvim/nvim/lua/plugins/treesj.lua
+++ 009_nvim/nvim/lua/plugins/treesj.lua
@@ -7,8 +7,6 @@ return {
       use_default_keymaps = false
     })
 
-    vim.keymap.set("n", "<leader>jt", "<cmd>TSJToggle<CR>", { desc = "TS[J] [T]oggle line split or join" })
-    vim.keymap.set("n", "<leader>js", "<cmd>TSJSplit<CR>", { desc = "TS[J] [S]plit line" })
-    vim.keymap.set("n", "<leader>jj", "<cmd>TSJJoin<CR>", { desc = "TS[J] [J]oin line" })
+    vim.keymap.set("n", "gS", "<cmd>TSJToggle<CR>", { desc = "Toggle line split or join" })
   end,
 }
