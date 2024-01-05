39b1ca6b1370ac1c50f7ec270c35e0e052293393
Danthewaann <daniel.black@slicelife.com>
2024-01-05 14:45:44 +0000

    Remove buf-only plugin

--- 009_nvim/nvim/lua/plugins/buf-only.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "numToStr/BufOnly.nvim",
-  event = "VeryLazy",
-  config = function ()
-    vim.keymap.set("n", "<leader>bo", "<cmd>BufOnly<CR>", { desc = "[B]uffer [O]nly" })
-  end
-}
