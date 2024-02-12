5a166501bf73dcc6c7d491028c26b0b18a765394
Danthewaann <daniel.black@slicelife.com>
2024-02-12 18:11:35 +0000

    Move <leader>ws to <leader>sa bind for workspace symbol search

--- 009_nvim/nvim/lua/plugins/nvim-lspconfig.lua
+++ 009_nvim/nvim/lua/plugins/nvim-lspconfig.lua
@@ -78,14 +78,14 @@ return {
       nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
       nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
       nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-      nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+      nmap("<leader>sa", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[S]earch [A]ll Workspace Symbols")
       nmap("<leader>ss", function()
         local word = vim.fn.expand("<cword>")
         require("telescope.builtin").lsp_workspace_symbols({
           prompt_title = "LSP Workspace Symbols (" .. word .. ")",
           query = word,
         })
-      end, "[S]earch [S]ymbols")
+      end, "[S]earch Current Workspace [S]ymbol")
 
       -- See `:help K` for why this keymap
       nmap("K", function()
