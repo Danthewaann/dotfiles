a5c116fd05187596d9bc4c0489d574837656a15d
Danthewaann <daniel.black@slicelife.com>
2024-02-19 16:17:53 +0000

    Remove commitlint

--- 009_nvim/nvim/lua/plugins/nvim-lint.lua
+++ 009_nvim/nvim/lua/plugins/nvim-lint.lua
@@ -5,12 +5,10 @@ return {
     require("lint").linters_by_ft = {
       python = { "mypy" },
       go = { "golangcilint" },
-      gitcommit = { "commitlint" }
     }
 
     local utils = require("custom.utils")
     local mypy_linter = require("lint").linters.mypy
-    local commit_linter = require("lint").linters.commitlint
 
     -- Use mypy in virtual environment if found
     mypy_linter.cmd = utils.get_poetry_venv_executable_path("mypy")
@@ -30,15 +28,6 @@ return {
       table.insert(mypy_linter.args, "--disable-error-code")
       table.insert(mypy_linter.args, code)
     end
-
-    -- From: https://github.com/conventional-changelog/commitlint/issues/613#issuecomment-1061807137
-    local npm_root = vim.fn.trim(vim.fn.system("npm root -g"))
-    commit_linter.args = {
-      "-g",
-      os.getenv("HOME") .. "/.commitlintrc.yaml",
-      "-x",
-      npm_root .. "/@commitlint/config-conventional",
-    }
 
     vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost", "InsertLeave" }, {
       callback = function()
