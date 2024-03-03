6bd8ea9c8de00b6c980dd97d19b573be874dd485
Danthewaann <danielcrblack@gmail.com>
2024-03-03 12:19:07 +0000

    Update docker install steps

--- 006_docker/install.sh
+++ 006_docker/install.sh
@@ -12,7 +12,7 @@ if inside_wsl; then
     exit 0
 fi
 
-# Install docker engine 
+# Install docker engine
 if [[ $OSTYPE == "darwin"* ]]; then
     # From https://dhwaneetbhatt.com/blog/run-docker-without-docker-desktop-on-macos
     # TODO: Need to fully test this
@@ -38,14 +38,14 @@ else
     if ! grep -i "docker" /etc/apt/sources.list.d/docker.list > /dev/null 2>&1; then
         run_command "adding /etc/apt/keyrings/docker.gpg to -> /etc/apt/sources.list.d/docker.list" \
             "echo deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \\
-            $(lsb_release -cs) stable > /etc/apt/sources.list.d/docker.list"
+            $(lsb_release -cs) stable | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
     fi
 
     # Install the docker engine
     run_command "installing the docker engine and cli" \
                 "sudo apt-get update -y && \\
                 sudo apt-get install -y docker-ce docker-ce-cli \\
-                containerd.io docker-compose-plugin"
+                containerd.io docker-buildx-plugin docker-compose-plugin"
 
     # Make sure the docker group exists
     if ! getent group docker > /dev/null 2>&1; then
