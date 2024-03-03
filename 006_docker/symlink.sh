7c8325914b2b477964da255738d343b005d6e0a8
Danthewaann <danielcrblack@gmail.com>
2024-03-03 12:22:20 +0000

    Add template docker config file

--- 006_docker/symlink.sh
+++ 006_docker/symlink.sh
@@ -9,4 +9,8 @@ source "$SCRIPT_DIR"/../common
 
 mkdir -p "$HOME/.docker"
 
+if [[ ! -f "$SCRIPT_DIR/config.json" ]]; then
+    cp "$SCRIPT_DIR/config.template.json" "$SCRIPT_DIR/config.json" 
+fi
+
 echo "$SCRIPT_DIR/config.json" "$HOME/.docker/config.json"
