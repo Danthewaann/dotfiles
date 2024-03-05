7e319845d2fedc1e4e76701f2ebec068d36d110c
Danthewaann <daniel.black@slicelife.com>
2024-03-05 15:53:10 +0000

    Set max_lines to 1 for nvim-treesitter-context plugin

--- 009_nvim/nvim/lua/plugins/nvim-treesitter-context.lua
+++ 009_nvim/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   config = function()
+    require("treesitter-context").setup({
+      max_lines = 1
+    })
+
     vim.keymap.set("n", "<leader>k", function()
       require("treesitter-context").go_to_context()
     end, { silent = true, desc = "Jump to context" })
