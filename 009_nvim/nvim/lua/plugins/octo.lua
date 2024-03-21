7859cbf2b4f5067e9c4294481150af94bae508e1
Danthewaann <daniel.black@slicelife.com>
2024-03-21 18:06:40 +0000

    Customise binds for octo

--- 009_nvim/nvim/lua/plugins/octo.lua
+++ 009_nvim/nvim/lua/plugins/octo.lua
@@ -8,10 +8,38 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+    local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+
     require("octo").setup({
       suppress_missing_scope = {
         projects_v2 = true,
+      },
+      mappings = {
+        review_thread = {
+          close_review_tab = { lhs = "" }
+        },
+        review_diff = {
+          close_review_tab = { lhs = "" }
+        },
+        file_panel = {
+          close_review_tab = { lhs = "" },
+        }
       }
+    })
+
+    augroup("octo", { clear = true })
+    autocmd("FileType", {
+      group = "octo",
+      pattern = "octo_panel",
+      callback = function()
+        vim.keymap.set(
+          "n",
+          "<leader>vc",
+          "<cmd> Octo review commit<CR>",
+          { buffer = true, desc = "review commit" }
+        )
+      end,
     })
   end
 }
