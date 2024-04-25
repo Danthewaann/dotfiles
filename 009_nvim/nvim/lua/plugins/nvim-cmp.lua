366515a291fe3033b77aa39bbb12fc6ea9f8b355
Danthewaann <daniel.black@slicelife.com>
2024-04-25 11:40:45 +0100

    Add fallbacks to <C-l> and <C-h> cmp binds

--- 009_nvim/nvim/lua/plugins/nvim-cmp.lua
+++ 009_nvim/nvim/lua/plugins/nvim-cmp.lua
@@ -124,14 +124,18 @@ return {
         }),
         ["<C-q>"] = cmp.mapping.abort(),
         ["<C-space>"] = cmp.mapping.complete(),
-        ["<C-l>"] = cmp.mapping(function()
+        ["<C-l>"] = cmp.mapping(function(fallback)
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
+          else
+            fallback()
           end
         end, { "i", "s" }),
-        ["<C-h>"] = cmp.mapping(function()
+        ["<C-h>"] = cmp.mapping(function(fallback)
           if luasnip.locally_jumpable(-1) then
             luasnip.jump(-1)
+          else
+            fallback()
           end
         end, { "i", "s" }),
       }),
