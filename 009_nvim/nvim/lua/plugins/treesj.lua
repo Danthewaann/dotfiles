eb2706ef1b900ef4104d7418a7b820586fde1087
Danthewaann <daniel.black@slicelife.com>
2024-08-21 09:19:00 +0100

    Increase max_join_length in treesj plugin

--- 009_nvim/nvim/lua/plugins/treesj.lua
+++ 009_nvim/nvim/lua/plugins/treesj.lua
@@ -4,7 +4,8 @@ return {
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
     require("treesj").setup({
-      use_default_keymaps = false
+      use_default_keymaps = false,
+      max_join_length = 200
     })
 
     vim.keymap.set("n", "gS", "<cmd>TSJToggle<CR>", { desc = "Toggle line split or join" })
