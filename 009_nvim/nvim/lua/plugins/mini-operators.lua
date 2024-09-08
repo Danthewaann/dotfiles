722b3bf7adf74f7635dd99167924aa7a0a222b80
Danthewaann <danielcrblack@gmail.com>
2024-09-08 12:09:36 +0100

    Adjust mini-operator binds

--- 009_nvim/nvim/lua/plugins/mini-operators.lua
+++ 009_nvim/nvim/lua/plugins/mini-operators.lua
@@ -3,8 +3,20 @@ return {
   version = "*",
   config = function()
     require("mini.operators").setup({
+      evaluate = {
+        prefix = "g="
+      },
+      exchange = {
+        prefix = "ge"
+      },
+      multiply = {
+        prefix = "m"
+      },
       replace = {
         prefix = "s"
+      },
+      sort = {
+        prefix = "go"
       }
     })
   end
