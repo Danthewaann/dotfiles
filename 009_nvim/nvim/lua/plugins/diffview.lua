8877dc413448c3e59bd4540d3471a61342b63c74
Danthewaann <daniel.black@slicelife.com>
2024-09-24 16:31:18 +0100

    Add file_history_panel binds in diffview

--- 009_nvim/nvim/lua/plugins/diffview.lua
+++ 009_nvim/nvim/lua/plugins/diffview.lua
@@ -20,6 +20,20 @@ return {
             { desc = "Select previous entry" }
           }
         },
+        file_history_panel = {
+          {
+            "n",
+            "<C-w><C-j>",
+            function() actions.select_next_entry() end,
+            { desc = "Select next entry" }
+          },
+          {
+            "n",
+            "<C-w><C-k>",
+            function() actions.select_prev_entry() end,
+            { desc = "Select previous entry" }
+          }
+        },
         view = {
           {
             "n",
