d660862463493c0f964a888837146a2ac0689904
Danthewaann <daniel.black@slicelife.com>
2024-11-15 12:54:33 +0000

    Combine all diffview commands into <leader>dv bind

--- 009_nvim/nvim/lua/plugins/diffview.lua
+++ 009_nvim/nvim/lua/plugins/diffview.lua
@@ -60,45 +60,48 @@ return {
     })
 
     vim.keymap.set("n", "<leader>dv", function()
+      local commands = {
+        ["Diff working tree against HEAD"] = function()
+          vim.cmd(":DiffviewOpen")
+        end,
+        ["Diff HEAD against origin merge base"] = function()
+          local base_branch = vim.fn.trim(vim.fn.system("git-get-base-branch"))
+          if vim.v.shell_error ~= 0 then
+            M.print_err(base_branch)
+            return
+          end
+
+          vim.cmd(":DiffviewOpen origin/" .. base_branch .. "...HEAD")
+        end,
+        ["View all file history "] = function()
+          vim.cmd(":DiffviewFileHistory")
+        end,
+        ["View current file history "] = function()
+          vim.cmd(":DiffviewFileHistory %")
+        end,
+      }
+
+      local keys = vim.tbl_keys(commands)
+      table.sort(keys)
+
       vim.ui.select(
-        { "Diff working tree against HEAD", "Diff HEAD against origin merge base", "Custom" },
+        keys,
         { prompt = "Choose Diffview" },
         function(choice)
-          if choice == "Diff working tree against HEAD" then
-            vim.cmd(":DiffviewOpen")
-          elseif choice == "Diff HEAD against origin merge base" then
-            local base_branch = vim.fn.trim(vim.fn.system("git-get-base-branch"))
-            if vim.v.shell_error ~= 0 then
-              M.print_err(base_branch)
+          for key, value in pairs(commands) do
+            if choice == key then
+              value()
               return
             end
-
-            vim.cmd(":DiffviewOpen origin/" .. base_branch .. "...HEAD")
-          elseif choice == "Custom" then
-            vim.ui.input(
-            { prompt = "Enter DiffviewOpen arguments" },
-              function(input)
-                if input ~= nil then
-                  vim.cmd(":DiffviewOpen " .. input)
-                end
-              end)
           end
         end
       )
     end, {
       desc = "[D]iff [V]iew",
     })
-    vim.keymap.set("n", "<leader>gla", ":DiffviewFileHistory<CR>", {
+    vim.keymap.set("x", "<leader>dv", ":DiffviewFileHistory<CR>", {
       silent = true,
-      desc = "[G]it [L]og [A]ll"
-    })
-    vim.keymap.set("n", "<leader>glf", ":DiffviewFileHistory %<CR>", {
-      silent = true,
-      desc = "[G]it [L]og [F]ile"
-    })
-    vim.keymap.set("x", "<leader>gl", ":DiffviewFileHistory<CR>", {
-      silent = true,
-      desc = "[G]it [L]og for selection"
+      desc = "[D]iff [V]iew file history for selection"
     })
   end,
 }
