255c130e6580e7137ebea5a1abd575684278cd54
Danthewaann <daniel.black@slicelife.com>
2024-11-24 13:52:12 +0000

    Add output messages to buffer_manager binds

--- 009_nvim/nvim/lua/plugins/buffer_manager.lua
+++ 009_nvim/nvim/lua/plugins/buffer_manager.lua
@@ -9,9 +9,11 @@ return {
 
     vim.keymap.set("n", "<leader>bw", function()
       buf_ui.save_menu_to_file(".nvim_buffers")
+      utils.print("Written buffers list to .nvim_buffers")
     end, { desc = "[B]uffers [W]rite" })
     vim.keymap.set("n", "<leader>bl", function()
       buf_ui.load_menu_from_file(".nvim_buffers")
+      utils.print("Loaded buffers list from .nvim_buffers")
     end, { desc = "[B]uffers [L]oad" })
     vim.keymap.set("n", "<C-b>", buf_ui.toggle_quick_menu, { desc = "Toggle buffers list" })
     vim.keymap.set("n", "<C-l>", buf_ui.nav_next, { desc = "Next buffer" })
