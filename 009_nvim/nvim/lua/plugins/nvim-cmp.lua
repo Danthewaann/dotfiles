31940d6ec2c944d9fc052d0aaff912388ed4a621
Danthewaann <daniel.black@slicelife.com>
2024-12-03 18:19:56 +0000

    Re-enable auto completion in cmdline

--- 009_nvim/nvim/lua/plugins/nvim-cmp.lua
+++ 009_nvim/nvim/lua/plugins/nvim-cmp.lua
@@ -103,9 +103,22 @@ return {
       }),
       matching = { disallow_symbol_nonprefix_matching = false },
       sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-      completion = {
-        autocomplete = false
-      },
+      enabled = function()
+        -- Set of commands where cmp will be disabled
+        local disabled = {
+          W = true,
+          w = true,
+          wa = true,
+          wq = true,
+          q = true,
+          qa = true,
+        }
+        -- Get first word of cmdline
+        local cmd = vim.fn.getcmdline():match("%S+")
+        -- Return true if cmd isn't disabled
+        -- else call/return cmp.close(), which returns false
+        return not disabled[cmd] or cmp.close()
+      end
     })
 
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
