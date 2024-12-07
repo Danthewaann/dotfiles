edbd3cf7155c4918a92f50f180d45b3c2e64592b
Danthewaann <daniel.black@slicelife.com>
2024-12-07 17:35:45 +0000

    Adjust config for quicker

--- 009_nvim/nvim/lua/plugins/quicker.lua
+++ 009_nvim/nvim/lua/plugins/quicker.lua
@@ -5,7 +5,36 @@ return {
     require("quicker").setup({
       opts = {
         number = true
-      }
+      },
+      keys = {
+        {
+          ">",
+          function()
+            require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+          end,
+          desc = "Expand quickfix context",
+        },
+        {
+          "<",
+          function()
+            require("quicker").collapse()
+          end,
+          desc = "Collapse quickfix context",
+        },
+        {
+          "r",
+          require("quicker").refresh,
+          desc = "Refresh the quickfix list"
+        }
+      },
+      highlight = {
+        -- Use treesitter highlighting
+        treesitter = true,
+        -- Use LSP semantic token highlighting
+        lsp = false,
+        -- Load the referenced buffers to apply more accurate highlights (may be slow)
+        load_buffers = false,
+      },
     })
   end
 }
