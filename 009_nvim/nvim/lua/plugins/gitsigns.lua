df54aaae50ad1dfe8c7257210eedf4356cd26cf0
Danthewaann <daniel.black@slicelife.com>
2024-12-14 12:22:40 +0000

    Adjust git signs

--- 009_nvim/nvim/lua/plugins/gitsigns.lua
+++ 009_nvim/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,13 @@ return {
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   "lewis6991/gitsigns.nvim",
   opts = {
+    signs = {
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+    },
     -- See `:help gitsigns.txt`
     preview_config = {
       -- Options passed to nvim_open_win
