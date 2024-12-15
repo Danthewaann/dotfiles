b737f1e8a7e191467718092333c1577c8a8bb3e3
Danthewaann <daniel.black@slicelife.com>
2024-12-15 10:05:56 +0000

    Remove custom NeogitWinSeparator highlight as it has been fixed upstream

--- 009_nvim/nvim/lua/custom/colourscheme.lua
+++ 009_nvim/nvim/lua/custom/colourscheme.lua
@@ -103,6 +103,3 @@ vim.cmd("highlight! link DiagnosticFloatingOk DiagnosticOk")
 vim.cmd("highlight! link LazyBackdrop Normal")
 vim.cmd("highlight TabLineSel guifg=#abb2bf guibg=#272731")
 vim.cmd("highlight TabLine guifg=#828997")
-
--- From: https://github.com/NeogitOrg/neogit/issues/1371
-vim.cmd("highlight! link NeogitWinSeparator WinSeparator")
