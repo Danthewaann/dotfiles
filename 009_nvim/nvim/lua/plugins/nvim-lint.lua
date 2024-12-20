21ecddfa371224051391621f56fdcbe1119c8ef1
Danthewaann <daniel.black@slicelife.com>
2024-12-20 21:26:40 +0000

    Use dmypy linter from nvim-lint linters

--- 009_nvim/nvim/lua/plugins/nvim-lint.lua
+++ 009_nvim/nvim/lua/plugins/nvim-lint.lua
@@ -9,30 +9,10 @@ return {
 
     local utils = require("custom.utils")
 
-    -- From: https://github.com/mfussenegger/nvim-lint/blob/master/lua/lint/linters/mypy.lua
-    local pattern = "([^:]+):(%d+):(%d+):(%d+):(%d+): (%a+): (.*) %[(%a[%a-]+)%]"
-    local groups = { "file", "lnum", "col", "end_lnum", "end_col", "severity", "message", "code" }
-    local severities = {
-      error = vim.diagnostic.severity.ERROR,
-      warning = vim.diagnostic.severity.WARN,
-      note = vim.diagnostic.severity.HINT,
-    }
-
-    require("lint").linters.dmypy = {
-      cmd = utils.get_poetry_venv_executable_path("dmypy"),
-      stdin = false,
-      append_fname = false,
-      ignore_exitcode = true,
-      stream = "both",
-      args = utils.dmypy_args(),
-      parser = require("lint.parser").from_pattern(
-        pattern,
-        groups,
-        severities,
-        { ["source"] = "mypy" },
-        { end_col_offset = 0 }
-      )
-    }
+    local dmypy_linter = require("lint").linters.dmypy
+    dmypy_linter.cmd = utils.get_poetry_venv_executable_path("dmypy")
+    dmypy_linter.append_fname = false
+    dmypy_linter.args = utils.dmypy_args()
 
     vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost", "InsertLeave" }, {
       callback = function()
