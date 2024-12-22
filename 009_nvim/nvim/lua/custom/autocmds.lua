c8a5f2b480c3b484bf2b71178e4605815b1cf406
Danthewaann <daniel.black@slicelife.com>
2024-12-22 11:36:17 +0000

    Adjust quickfix binds

--- 009_nvim/nvim/lua/custom/autocmds.lua
+++ 009_nvim/nvim/lua/custom/autocmds.lua
@@ -169,14 +169,14 @@ autocmd("FileType", {
 
     -- Replace current word in current file
     vim.keymap.set("n", "<leader>rp",
-      [[:cfdo %s/\<<C-r><C-w>\>/<C-r><C-w>/gI | update<Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left>]],
+      [[:cdo %s/\<<C-r><C-w>\>/<C-r><C-w>/gI | update<Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left>]],
       { buffer = 0, desc = "[R]e[p]lace current word in qf items" }
     )
 
     -- Replace visual selection in current file
     -- TODO: The below bind raises an error due to the `TextYankPost` autocmd I have above
     vim.keymap.set("v", "<leader>rp",
-      [["ky:cfdo %s/<C-r>=escape(@k, "/")<CR>/<C-r>=escape(@k, "/")<CR>/gI | update<Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left>]],
+      [["ky:cdo %s/<C-r>=escape(@k, "/")<CR>/<C-r>=escape(@k, "/")<CR>/gI | update<Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left><Left>]],
       { buffer = 0, desc = "[R]e[p]lace selection in qf items" }
     )
   end,
