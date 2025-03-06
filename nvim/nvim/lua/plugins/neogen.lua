30a7470ad38797d132005a626e10ed7d0ac71d1c
Danthewaann <daniel.black@slicelife.com>
2025-03-06 11:02:02 +0000

    Lazy load neogen and treesj plugins

--- nvim/nvim/lua/plugins/neogen.lua
+++ nvim/nvim/lua/plugins/neogen.lua
@@ -1,12 +1,14 @@
 return {
   "danymat/neogen",
-  event = "VeryLazy",
   dependencies = "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("neogen").setup({ snippet_engine = "luasnip" })
-
-    vim.keymap.set("n", "<leader>gd", require("neogen").generate, {
-      noremap = true, silent = true, desc = "[G]enerate [D]ocstring"
-    })
-  end,
+  keys = {
+    {
+      "<leader>gd",
+      function()
+        require("neogen").generate()
+      end,
+      desc = "[G]enerate [D]ocstring"
+    }
+  },
+  opts = { snippet_engine = "luasnip" }
 }
--- nvim/nvim/lua/plugins/treesj.lua
+++ nvim/nvim/lua/plugins/treesj.lua
@@ -1,13 +1,11 @@
 return {
   "Wansmer/treesj",
-  event = "VeryLazy",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    require("treesj").setup({
-      use_default_keymaps = false,
-      max_join_length = 200
-    })
-
-    vim.keymap.set("n", "gS", "<cmd>TSJToggle<CR>", { desc = "Toggle line split or join" })
-  end,
+  keys = {
+    { "gS", "<cmd>TSJToggle<CR>", desc = "Toggle line split or join" }
+  },
+  opts = {
+    use_default_keymaps = false,
+    max_join_length = 200
+  }
 }
