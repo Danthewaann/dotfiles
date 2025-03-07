98895885b9c0a809d00f31858b524568eaf6a267
Danthewaann <daniel.black@slicelife.com>
2025-03-07 17:55:54 +0000

    Load mkdnflow plugin only in markdown files

--- nvim/nvim/lua/plugins/mkdnflow.lua
+++ nvim/nvim/lua/plugins/mkdnflow.lua
@@ -1,26 +1,25 @@
 return {
   "jakewvincent/mkdnflow.nvim",
-  config = function()
-    require("mkdnflow").setup({
-      to_do = {
-        symbols = { " ", "-", "x" },
-        complete = "x"
-      },
-      mappings = {
-        MkdnEnter = { { "n", "v", "i" }, "<CR>" },
-        MkdnGoBack = false,
-        MkdnGoForward = false,
-        MkdnCreateLinkFromClipboard = { { "n", "v" }, "<leader>la" },
-        MkdnDestroyLink = { "n", "<leader>ld" },
-        MkdnIncreaseHeading = { "n", "<leader>=" },
-        MkdnDecreaseHeading = { "n", "<leader>-" },
-        MkdnTableNewRowBelow = { "n", "<leader>ir" },
-        MkdnTableNewRowAbove = { "n", "<leader>iR" },
-        MkdnTableNewColAfter = { "n", "<leader>ic" },
-        MkdnTableNewColBefore = { "n", "<leader>iC" },
-        MkdnFoldSection = false,
-        MkdnUnfoldSection = false,
-      }
-    })
-  end
+  ft = "markdown",
+  opts = {
+    to_do = {
+      symbols = { " ", "-", "x" },
+      complete = "x"
+    },
+    mappings = {
+      MkdnEnter = { { "n", "v", "i" }, "<CR>" },
+      MkdnGoBack = false,
+      MkdnGoForward = false,
+      MkdnCreateLinkFromClipboard = { { "n", "v" }, "<leader>la" },
+      MkdnDestroyLink = { "n", "<leader>ld" },
+      MkdnIncreaseHeading = { "n", "<leader>=" },
+      MkdnDecreaseHeading = { "n", "<leader>-" },
+      MkdnTableNewRowBelow = { "n", "<leader>ir" },
+      MkdnTableNewRowAbove = { "n", "<leader>iR" },
+      MkdnTableNewColAfter = { "n", "<leader>ic" },
+      MkdnTableNewColBefore = { "n", "<leader>iC" },
+      MkdnFoldSection = false,
+      MkdnUnfoldSection = false,
+    }
+  },
 }
