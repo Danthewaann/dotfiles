a5303ec7453fbc25fc48e1fe41273bb86a11751e
Danthewaann <daniel.black@slicelife.com>
2025-03-10 18:38:26 +0000

    Refactor my custom breakpoints.lua code

--- nvim/nvim/lua/custom/breakpoints.lua
+++ nvim/nvim/lua/custom/breakpoints.lua
@@ -3,7 +3,6 @@ local utils = require("custom.utils")
 local ft_map = {
   go = {
     breakpoint_stmt = "runtime.Breakpoint()",
-    whitespace_char = "	",
     get_all_cmd = ":silent grep runtime.Breakpoint\\(\\) -g \"*.go\" ./ ",
     post_set_hook = function()
       local cursor_line = vim.fn.line(".")
@@ -26,47 +25,15 @@ local ft_map = {
   },
   python = {
     breakpoint_stmt = "breakpoint()",
-    whitespace_char = " ",
     get_all_cmd = ":silent grep breakpoint\\(\\) -g \"*.py\" ./",
   },
   ruby = {
     breakpoint_stmt = "binding.pry",
-    whitespace_char = " ",
     get_all_cmd = ":silent grep binding.pry -g \"*.rb\" ./",
   }
 }
 
--- Custom stuff for adding breakpoint() statements
---
--- Partially from https://gist.github.com/berinhard/523420
-local function get_line_content_and_whitespace(line_num)
-  local line = vim.fn.getline(line_num)
-  local whitespace = vim.fn.strlen(vim.fn.matchstr(line, "^\\s*"))
-  return { content = line, whitespace = whitespace }
-end
-
 local function set_breakpoint()
-  local cur_line_num = vim.fn.line(".")
-  local cur_line = get_line_content_and_whitespace(cur_line_num)
-
-  local next_line = get_line_content_and_whitespace(cur_line_num + 1)
-  if cur_line.whitespace > 0 and next_line.whitespace > cur_line.whitespace then
-    cur_line.whitespace = next_line.whitespace
-  else
-    while cur_line.whitespace == 0 do
-      if cur_line_num == 0 then
-        break
-      end
-      cur_line_num = cur_line_num - 1
-      cur_line = get_line_content_and_whitespace(cur_line_num)
-    end
-
-    next_line = get_line_content_and_whitespace(cur_line_num + 1)
-    if cur_line.whitespace > 0 and next_line.whitespace > cur_line.whitespace then
-      cur_line.whitespace = next_line.whitespace
-    end
-  end
-
   local file_type = vim.fn.getbufvar(vim.api.nvim_get_current_buf(), "&filetype")
   local breakpoint_data = ft_map[file_type]
   if breakpoint_data == nil then
@@ -75,18 +42,10 @@ local function set_breakpoint()
   end
 
   local breakpoint_stmt = breakpoint_data.breakpoint_stmt
-  local whitespace_char = breakpoint_data.whitespace_char
   local post_set_hook = breakpoint_data.post_set_hook
 
-  local output = ""
-  for _ = 1, cur_line.whitespace do
-    output = output .. whitespace_char
-  end
-
-  output = output .. breakpoint_stmt
-  vim.fn.append(vim.fn.line("."), output)
   -- Move the cursor down and to the start of the line for the newly inserted breakpoint
-  vim.cmd("norm! j_")
+  vim.cmd("norm! o" .. breakpoint_stmt)
 
   if post_set_hook ~= nil then
     post_set_hook()
