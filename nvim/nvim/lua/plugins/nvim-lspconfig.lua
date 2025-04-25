c1f0df4adb7711484fd031d009e042c126fe10af
Danthewaann <daniel.black@slicelife.com>
2025-04-25 13:21:34 +0100

    Fix LSP client method calls

--- nvim/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/nvim/lua/plugins/nvim-lspconfig.lua
@@ -196,7 +196,7 @@ return {
           end
 
           -- Enable highlighting usages of the symbol under the cursor if the LSP server supports it
-          if client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
             local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
             vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
               buffer = event.buf,
@@ -220,7 +220,7 @@ return {
           end
 
           -- Enable inlay hints if the LSP server supports it
-          if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+          if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
             map("<leader>th", function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
             end, "[T]oggle Inlay [H]ints")
