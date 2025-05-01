b6f3a0ff8a332d1cd098eb3650ce73a55fda318e
Danthewaann <daniel.black@slicelife.com>
2025-05-01 16:50:16 +0100

    Re-add vim-rsi plugin

--- nvim/nvim/lua/plugins/init.lua
+++ nvim/nvim/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
   -- Detect tabstop and shiftwidth automatically
   "tpope/vim-sleuth",
+  "tpope/vim-rsi",
   -- Auto read buffers that have changes outside neovim
   "djoshea/vim-autoread",
   -- "gc" to comment visual regions/lines
