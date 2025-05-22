70fe1468c23805817e3824e5c27514a151be7545
Danthewaann <daniel.black@slicelife.com>
2025-05-22 06:20:39 +0100

    Lazy load icon-picker

--- nvim/nvim/lua/plugins/icon-picker.lua
+++ nvim/nvim/lua/plugins/icon-picker.lua
@@ -1,12 +1,7 @@
 return {
   "ziontee113/icon-picker.nvim",
-  event = "VeryLazy",
-  config = function()
-    require("icon-picker").setup({
-      disable_legacy_commands = true,
-    })
-    vim.keymap.set("i", "<C-p>", "<cmd>IconPickerInsert emoji<cr>",
-      { desc = "Pick an emoji to insert", noremap = true, silent = true }
-    )
-  end,
+  keys = {
+    { "<C-p>", "<cmd>IconPickerInsert emoji<CR>", desc = "Pick an emoji to insert", mode = "i" }
+  },
+  opts = { disable_legacy_commands = true }
 }
