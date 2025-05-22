6284ba14c4ba919926e6f17dab1574ab8ad90807
Danthewaann <daniel.black@slicelife.com>
2025-05-22 05:18:42 +0100

    Adjust octo.nvim binds

--- nvim/nvim/lua/plugins/octo.lua
+++ nvim/nvim/lua/plugins/octo.lua
@@ -1,6 +1,9 @@
 return {
   "Danthewaann/octo.nvim",
-  event = "VeryLazy",
+  keys = {
+    { "<leader>op", desc = "[O]cto open [P]R" },
+    { "<leader>ov", desc = "[O]cto start or resume PR re[v]iew" },
+  },
   cmd = "Octo",
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -11,11 +14,25 @@ return {
     vim.treesitter.language.register("markdown", "octo")
 
     require("octo").setup({
+      picker = "telescope",
       use_local_fs = true,
-      suppress_missing_scope = {
-        projects_v2 = true,
+      reviews = { auto_show_threads = false, focus = "right" },
+      ui = {
+        use_signcolumn = true, -- show "modified" marks on the sign column
+        use_signstatus = true, -- show "modified" marks on the status column
       },
+      suppress_missing_scope = { projects_v2 = true },
+      enable_builtin = true,
+      mappings_disable_default = false,
       mappings = {
+        pull_request = {
+          list_commits = { lhs = "<localleader>oc", desc = "list PR commits" },
+          list_changed_files = { lhs = "<localleader>of", desc = "list PR changed files" },
+          show_pr_diff = { lhs = "<localleader>od", desc = "show PR diff" },
+        },
+        submit_win = {
+          close_review_tab = { lhs = "" },
+        },
         review_thread = {
           close_review_tab = { lhs = "" },
           select_next_entry = { lhs = "<M-j>", desc = "move to previous changed file" },
@@ -33,18 +50,18 @@ return {
         }
       }
     })
+    vim.keymap.set("n", "<leader>op", function()
+      local buffers = vim.api.nvim_list_bufs()
+      for _, buf in ipairs(buffers) do
+        local buf_name = vim.api.nvim_buf_get_name(buf)
 
-    vim.api.nvim_create_autocmd("FileType", {
-      group = vim.api.nvim_create_augroup("octo", { clear = true }),
-      pattern = "octo_panel",
-      callback = function()
-        vim.keymap.set(
-          "n",
-          "<leader>vc",
-          "<cmd> Octo review commit<CR>",
-          { buffer = true, desc = "review commit" }
-        )
-      end,
-    })
+        if buf_name:match("^octo://") and vim.api.nvim_get_option_value("modified", { buf = buf }) then
+          vim.api.nvim_buf_delete(buf, { force = true })
+        end
+      end
+      vim.api.nvim_command("Octo pr")
+    end, { desc = "Open PR for current branch" })
+    vim.keymap.set("n", "<leader>ov", "<cmd>Octo review<CR>", { desc = "Start or resume review" })
+    vim.keymap.set("n", "<leader>ot", "<cmd>Octo review thread<CR>", { desc = "Show review threads" })
   end
 }
