eed06296d0a4833af73d0d6e5f540db0c96250f4
Danthewaann <daniel.black@slicelife.com>
2025-05-26 07:50:39 +0100

    Use keys in gitlinker.nvim plugin

--- nvim/nvim/lua/plugins/gitlinker.lua
+++ nvim/nvim/lua/plugins/gitlinker.lua
@@ -1,32 +1,10 @@
 return {
   "linrongbin16/gitlinker.nvim",
-  config = function()
-    require("gitlinker").setup()
-
-    vim.keymap.set(
-      { "n", "v" },
-      "<leader>gy",
-      "<cmd>GitLink<cr>",
-      { silent = true, noremap = true, desc = "Yank git permlink" }
-    )
-    vim.keymap.set(
-      { "n", "v" },
-      "<leader>gY",
-      "<cmd>GitLink!<cr>",
-      { silent = true, noremap = true, desc = "Open git permlink" }
-    )
-
-    vim.keymap.set(
-      { "n", "v" },
-      "<leader>gb",
-      "<cmd>GitLink blame<cr>",
-      { silent = true, noremap = true, desc = "Yank git blame link" }
-    )
-    vim.keymap.set(
-      { "n", "v" },
-      "<leader>gB",
-      "<cmd>GitLink! blame<cr>",
-      { silent = true, noremap = true, desc = "Open git blame link" }
-    )
-  end
+  keys = {
+    { "<leader>gy", "<cmd>GitLink<cr>",        mode = { "n", "v" }, desc = "Yank git link" },
+    { "<leader>gY", "<cmd>GitLink!<cr>",       mode = { "n", "v" }, desc = "Open git link" },
+    { "<leader>gb", "<cmd>GitLink blame<cr>",  mode = { "n", "v" }, desc = "Yank git blame link" },
+    { "<leader>gB", "<cmd>GitLink! blame<cr>", mode = { "n", "v" }, desc = "Open git blame link" },
+  },
+  opts = {}
 }
