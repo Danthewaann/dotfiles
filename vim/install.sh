4632669a954c6e66cc3cd680dcfe9d6977749492
Danthewaann <daniel.black@slicelife.com>
2025-06-01 09:59:20 +0100

    Clean up .vimrc to be as minimum as possible
    Since I've been using neovim for some time now, I want to make my .vimrc
    file for vim as small as possible so if one day I need to go back to
    doing some work on a remote server, I can just copy over my .vimrc to
    get some reasonable setup.

--- vim/install.sh
+++ vim/install.sh
@@ -10,23 +10,10 @@ source "$SCRIPT_DIR"/../common
 # Install vim 9
 if [[ $OSTYPE == "darwin"* ]]; then
     run_command "installing vim" \
-        "brew install macvim"
+        "brew install vim"
 else
     run_command "installing vim" \
         "sudo add-apt-repository -y ppa:jonathonf/vim" \
         "sudo apt-get install -y vim"
 fi
-
-# Install vim-plug plugin manager
-if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
-    run_command "installing vim-plug plugin manager" \
-        "curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \\
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-fi
-
-# Install vim plugins
-run_command "installing vim plugins" \
-    "vim --not-a-term +PlugInstall\! +qall > /dev/null 2>&1"
-
-run_command "creating $HOME/.coc-post directory" "mkdir -p $HOME/.coc-post"
 
--- vim/symlink.sh
+++ vim/symlink.sh
@@ -9,19 +9,3 @@ source "$SCRIPT_DIR"/../common
 
 echo "$SCRIPT_DIR/.vimrc" "$HOME/.vimrc"
 
-echo "$SCRIPT_DIR/coc-settings.json" "$HOME/coc-settings.json"
-
-mkdir -p "$XDG_CONFIG_HOME/clangd"
-echo "$SCRIPT_DIR/clangd_config.yaml" "$XDG_CONFIG_HOME/clangd/config.yaml"
-
-mkdir -p "$HOME/.vim/autoload"
-for d in "$SCRIPT_DIR"/autoload/*; do
-    if [[ -d "$d" ]]; then
-        echo "$d" "$HOME/.vim/autoload/$(basename "$d")"
-    fi
-done
-
-mkdir -p "$HOME/.vim/ftplugin"
-for f in "$SCRIPT_DIR"/ftplugin/*; do
-    echo "$f" "$HOME/.vim/ftplugin/$(basename "$f")"
-done
