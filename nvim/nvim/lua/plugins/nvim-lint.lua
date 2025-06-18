1b9d5f7028440e6065944175b5d0532ab9852247
Danthewaann <daniel.black@slicelife.com>
2025-06-18 15:52:10 +0100

    Add toggle to use mypy or dmypy in nvim-lint

--- nvim/nvim/lua/plugins/nvim-lint.lua
+++ nvim/nvim/lua/plugins/nvim-lint.lua
@@ -2,17 +2,26 @@ return {
   "mfussenegger/nvim-lint",
   event = "VeryLazy",
   config = function()
+    local use_dmypy = false
+    local mypy = "mypy"
+
+    if use_dmypy then
+      mypy = "dmypy"
+    end
+
     require("lint").linters_by_ft = {
-      python = { "dmypy" },
+      python = { mypy },
       go = { "golangcilint" },
     }
 
     local utils = require("custom.utils")
 
-    local dmypy_linter = require("lint").linters.dmypy
-    dmypy_linter.cmd = utils.get_poetry_venv_executable_path("dmypy")
-    dmypy_linter.append_fname = false
-    dmypy_linter.args = utils.dmypy_args()
+    local mypy_linter = require("lint").linters.mypy
+    if use_dmypy then
+      mypy_linter = require("lint").linters.dmypy
+    end
+
+    mypy_linter.cmd = utils.get_poetry_venv_executable_path(mypy)
 
     vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost", "InsertLeave" }, {
       callback = function()
