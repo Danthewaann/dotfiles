03ce2f6d853cac413c95db1c0a93535b479a31d5
Danthewaann <daniel.black@slicelife.com>
2025-06-20 20:28:46 +0100

    Remove mypy from nvim-lint

--- nvim/nvim/lua/plugins/nvim-lint.lua
+++ nvim/nvim/lua/plugins/nvim-lint.lua
@@ -2,28 +2,11 @@ return {
   "mfussenegger/nvim-lint",
   event = "VeryLazy",
   config = function()
-    local use_dmypy = false
-    local mypy = "mypy"
-
-    if use_dmypy then
-      mypy = "dmypy"
-    end
-
     require("lint").linters_by_ft = {
-      python = { mypy },
       go = { "golangcilint" },
     }
 
-    local utils = require("custom.utils")
-
-    local mypy_linter = require("lint").linters.mypy
-    if use_dmypy then
-      mypy_linter = require("lint").linters.dmypy
-    end
-
-    mypy_linter.cmd = utils.get_poetry_venv_executable_path(mypy)
-
-    vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost" }, {
+    vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost", "InsertLeave" }, {
       callback = function()
         -- Only run the linter in buffers that you can modify in order to
         -- avoid superfluous noise, notably within the handy LSP pop-ups that
