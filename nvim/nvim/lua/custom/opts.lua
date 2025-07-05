7129c95e715626b2aa9192d885bc6056cb170be1
Danthewaann <daniel.black@slicelife.com>
2025-07-05 21:39:51 +0100

    Fix nvim argv check for shadafile setup

--- nvim/nvim/lua/custom/opts.lua
+++ nvim/nvim/lua/custom/opts.lua
@@ -111,7 +111,7 @@ vim.o.wildmode = "longest:full,full"
 
 -- Setup project specific shada file to store marks, history
 -- for just the current project if we can create the `.vim` folder.
-if #vim.fn.argv() == 0 and vim.uv.fs_mkdir(".vim", tonumber("755", 8)) then
+if #vim.v.argv == 2 then
   vim.opt.shadafile = ".vim/project.shada"
 else
   -- Disable the shadafile in all other cases
