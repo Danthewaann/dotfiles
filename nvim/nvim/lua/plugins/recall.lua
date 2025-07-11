df367e01a803525505cd7b219b2ce1a4bfdb155d
Danthewaann <daniel.black@slicelife.com>
2025-07-11 05:47:39 +0100

    Add zz after recall jump binds

--- nvim/nvim/lua/plugins/recall.lua
+++ nvim/nvim/lua/plugins/recall.lua
@@ -3,8 +3,8 @@ return {
   config = function()
     require("recall").setup({})
     vim.keymap.set("n", "<leader>mm", "<cmd>RecallToggle<CR>", { desc = "[M]ark [M]ake" })
-    vim.keymap.set("n", "<M-l>", "<cmd>RecallNext<CR>", { desc = "Jump to next mark" })
-    vim.keymap.set("n", "<M-h>", "<cmd>RecallPrevious<CR>", { desc = "Jump to last mark" })
+    vim.keymap.set("n", "<M-l>", "<cmd>RecallNext<CR>zz", { desc = "Jump to next mark" })
+    vim.keymap.set("n", "<M-h>", "<cmd>RecallPrevious<CR>zz", { desc = "Jump to last mark" })
     vim.keymap.set("n", "<leader>mc", "<cmd>RecallClear<CR>", { desc = "[M]ark [C]lear all" })
     vim.keymap.set("n", "<leader>sm", "<cmd>Telescope recall<CR>", { desc = "[S]earch [M]arks" })
   end
