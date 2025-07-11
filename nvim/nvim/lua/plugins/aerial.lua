161f0c172bde3a62ac257bf612d8ab4a51423531
Danthewaann <daniel.black@slicelife.com>
2025-07-11 19:56:29 +0100

    Add <C-b> bind to toggle aerial window

--- nvim/nvim/lua/plugins/aerial.lua
+++ nvim/nvim/lua/plugins/aerial.lua
@@ -6,6 +6,7 @@ return {
       -- Jump forwards/backwards with '{' and '}'
       vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
       vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "<C-b>", "<cmd>AerialToggle!<CR>", { buffer = bufnr })
     end
   },
   -- Optional dependencies
