8df15087ee047714f7b5ddc536723dbb4f61005d
Danthewaann <danielcrblack@gmail.com>
2025-08-21 15:09:34 +0100

    Remove icon-picker nvim plugin

--- nvim/nvim/lua/plugins/icon-picker.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "ziontee113/icon-picker.nvim",
-  keys = {
-    { "<C-p>", "<cmd>IconPickerInsert emoji<CR>", desc = "Pick an emoji to insert", mode = "i" }
-  },
-  opts = { disable_legacy_commands = true }
-}
