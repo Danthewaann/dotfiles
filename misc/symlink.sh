2dd4075fc0aa3fdc6c10cb13ad24c458ec22eb73
Danthewaann <daniel.black@slicelife.com>
2025-09-03 13:35:19 +0100

    Handle linux only symlinks

--- misc/symlink.sh
+++ misc/symlink.sh
@@ -9,16 +9,18 @@ source "$SCRIPT_DIR"/../common
 
 echo "$SCRIPT_DIR/.jq" "$HOME/.jq"
 
-echo "$SCRIPT_DIR/keyd.conf" "/etc/keyd/default.conf" "true"
+if [[ $OSTYPE != "darwin"* ]]; then
+    echo "$SCRIPT_DIR/keyd.conf" "/etc/keyd/default.conf" "true"
 
-echo "$SCRIPT_DIR/i3" "$XDG_CONFIG_HOME/i3"
+    echo "$SCRIPT_DIR/i3" "$XDG_CONFIG_HOME/i3"
 
-echo "$SCRIPT_DIR/rofi" "$XDG_CONFIG_HOME/rofi"
+    echo "$SCRIPT_DIR/rofi" "$XDG_CONFIG_HOME/rofi"
 
-echo "$SCRIPT_DIR/polybar" "$XDG_CONFIG_HOME/polybar"
+    echo "$SCRIPT_DIR/polybar" "$XDG_CONFIG_HOME/polybar"
 
-echo "$SCRIPT_DIR/lockscreen" "$HOME/.local/bin/lockscreen"
+    echo "$SCRIPT_DIR/lockscreen" "$HOME/.local/bin/lockscreen"
 
-echo "$SCRIPT_DIR/dunst" "$XDG_CONFIG_HOME/dunst"
+    echo "$SCRIPT_DIR/dunst" "$XDG_CONFIG_HOME/dunst"
 
-echo "$SCRIPT_DIR/picom" "$XDG_CONFIG_HOME/picom"
+    echo "$SCRIPT_DIR/picom" "$XDG_CONFIG_HOME/picom"
+fi
