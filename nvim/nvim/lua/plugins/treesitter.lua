50b5a24d6dbb1b43e2308d7ada8d05a11d8fbd95
Danthewaann <daniel.black@slicelife.com>
2025-09-11 21:27:54 +0100

    Switch to main branch for nvim-treesitter plugin

--- nvim/nvim/lua/plugins/treesitter.lua
+++ nvim/nvim/lua/plugins/treesitter.lua
@@ -1,127 +1,52 @@
 return {
-  -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
+  branch = "main",
   build = ":TSUpdate",
   config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup({
-      -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = {
-        "c",
-        "cpp",
-        "go",
-        "gomod",
-        "gosum",
-        "lua",
-        "python",
-        "rust",
-        "tsx",
-        "javascript",
-        "typescript",
-        "vimdoc",
-        "vim",
-        "bash",
-        "regex",
-        "query",
-        "ruby",
-        "http",
-        "json",
-        "markdown",
-        "markdown_inline",
-        "diff",
-        "gitcommit",
-        "make",
-        "dockerfile",
-        "yaml",
-        "sql",
-        "toml",
-        "terraform",
-        "objc",
-        "swift",
-        "just",
-        "tmux"
-      },
+    local parsers = {
+      "c",
+      "cpp",
+      "go",
+      "gomod",
+      "gosum",
+      "lua",
+      "python",
+      "rust",
+      "tsx",
+      "javascript",
+      "typescript",
+      "vimdoc",
+      "vim",
+      "bash",
+      "regex",
+      "query",
+      "ruby",
+      "http",
+      "json",
+      "markdown",
+      "markdown_inline",
+      "diff",
+      "gitcommit",
+      "make",
+      "yaml",
+      "sql",
+      "toml",
+      "terraform",
+      "objc",
+      "swift",
+      "just",
+    }
 
-      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-      auto_install = false,
+    require("nvim-treesitter").install(parsers)
 
-      highlight = {
-        enable = true,
-        disable = function(_, buf)
-          -- Disable highlighting for large files
-          local max_filesize = 200 * 1024 -- 200 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-
-          local filetype = vim.bo[buf].filetype
-          if filetype == "dockerfile" or filetype == "tmux" then
-            return true
-          end
-        end,
-      },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          node_incremental = "v",
-          node_decremental = "V",
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["ah"] = "@assignment.lhs",
-            ["al"] = "@assignment.rhs",
-          }
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]]"] = "@class.outer",
-            ["]a"] = "@parameter.inner",
-          },
-          goto_next_end = {
-            ["]F"] = "@function.outer",
-            ["]["] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[["] = "@class.outer",
-            ["[a"] = "@parameter.inner",
-          },
-          goto_previous_end = {
-            ["[F"] = "@function.outer",
-            ["[]"] = "@class.outer",
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["g]"] = "@parameter.inner",
-            ["g}"] = "@function.outer",
-          },
-          swap_previous = {
-            ["g["] = "@parameter.inner",
-            ["g{"] = "@function.outer",
-          },
-        },
-      },
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function()
+        -- Syntax highlighting
+        vim.treesitter.start()
+        -- Folds
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      end,
     })
   end
 }
