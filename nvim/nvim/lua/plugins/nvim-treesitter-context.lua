2f679f1fa976f4fcfbad2464b2b572dd16dbe7c6
Danthewaann <daniel.black@slicelife.com>
2025-10-02 18:46:56 +0100

    Add separator to nvim-treesitter-context

--- nvim/nvim/lua/plugins/nvim-treesitter-context.lua
+++ nvim/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   config = function()
-    require("treesitter-context").setup()
+    require("treesitter-context").setup({ separator = "⎯", })
 
     vim.keymap.set("n", "<leader>k", function()
       require("treesitter-context").go_to_context()
