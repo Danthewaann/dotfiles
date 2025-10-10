2eaa6d763f01ad80108b3b4da41e656b9a84cd03
Danthewaann <daniel.black@slicelife.com>
2025-10-10 07:25:44 +0100

    Adjust nvim-treesitter-context config

--- nvim/nvim/lua/plugins/nvim-treesitter-context.lua
+++ nvim/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   config = function()
-    require("treesitter-context").setup({ separator = "⎯", })
+    require("treesitter-context").setup({ multiline_threshold = 1 })
 
     vim.keymap.set("n", "<leader>k", function()
       require("treesitter-context").go_to_context()
