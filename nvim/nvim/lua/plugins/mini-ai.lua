bcdf38f210ffc10fb771b8a519d745a650229462
Danthewaann <daniel.black@slicelife.com>
2025-12-14 12:14:34 +0000

    Add mini-extra.nvim plugin

--- nvim/nvim/lua/plugins/mini-ai.lua
+++ nvim/nvim/lua/plugins/mini-ai.lua
@@ -2,6 +2,16 @@ return {
   "echasnovski/mini.ai",
   version = "*",
   config = function()
-    require("mini.ai").setup({ n_lines = 500 })
+    local gen_ai_spec = require("mini.extra").gen_ai_spec
+    require("mini.ai").setup({
+      n_lines = 500,
+      custom_textobjects = {
+        B = gen_ai_spec.buffer(),
+        D = gen_ai_spec.diagnostic(),
+        I = gen_ai_spec.indent(),
+        L = gen_ai_spec.line(),
+        N = gen_ai_spec.number(),
+      },
+    })
   end
 }
