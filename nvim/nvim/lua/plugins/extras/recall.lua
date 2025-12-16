255a0fbd8194f393c21149c3a000419661c0a884
Danthewaann <daniel.black@slicelife.com>
2025-12-16 09:33:45 +0000

    Update recall.nvim plugin

--- nvim/nvim/lua/plugins/extras/recall.lua
+++ nvim/nvim/lua/plugins/extras/recall.lua
@@ -1,7 +1,7 @@
 return {
   "Danthewaann/recall.nvim",
   config = function()
-    require("recall").setup({})
+    require("recall").setup({ relative_marks = true })
     vim.keymap.set("n", "<leader>mm", "<cmd>RecallToggle<CR>", { desc = "[M]ark [M]ake" })
     vim.keymap.set("n", "<M-l>", "<cmd>RecallNext<CR>zz", { desc = "Jump to next mark" })
     vim.keymap.set("n", "<M-h>", "<cmd>RecallPrevious<CR>zz", { desc = "Jump to last mark" })
