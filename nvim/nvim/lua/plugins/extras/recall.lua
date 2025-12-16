759b03bbd194997722994b7d94d46a7ae52617c5
Danthewaann <daniel.black@slicelife.com>
2025-12-16 15:49:02 +0000

    Lazy load recall.nvim plugin

--- nvim/nvim/lua/plugins/extras/recall.lua
+++ nvim/nvim/lua/plugins/extras/recall.lua
@@ -1,11 +1,21 @@
 return {
   "Danthewaann/recall.nvim",
-  config = function()
-    require("recall").setup({ relative_marks = true })
-    vim.keymap.set("n", "<leader>mm", "<cmd>RecallToggle<CR>", { desc = "[M]ark [M]ake" })
-    vim.keymap.set("n", "<M-l>", "<cmd>RecallNext<CR>zz", { desc = "Jump to next mark" })
-    vim.keymap.set("n", "<M-h>", "<cmd>RecallPrevious<CR>zz", { desc = "Jump to last mark" })
-    vim.keymap.set("n", "<leader>mc", "<cmd>RecallClear<CR>", { desc = "[M]ark [C]lear all" })
-    vim.keymap.set("n", "<leader>sm", "<cmd>Telescope recall<CR>", { desc = "[S]earch [M]arks" })
-  end
+  opts = {},
+  keys = {
+    {
+      "<leader>mm", "<cmd>RecallToggle<CR>", desc = "[M]ark [M]ake"
+    },
+    {
+      "<leader>mc", "<cmd>RecallClear<CR>", desc = "[M]ark [C]lear"
+    },
+    {
+      "<leader>sm", "<cmd>Telescope recall<CR>", desc = "[S]earch [M]arks"
+    },
+    {
+      "<M-h>", "<cmd>RecallPrevious<CR>zz", desc = "Jump to previous mark"
+    },
+    {
+      "<M-l>", "<cmd>RecallNext<CR>zz", desc = "Jump to next mark"
+    },
+  }
 }
