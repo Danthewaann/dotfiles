b1a88e6be939af462882adda4c75353acc373236
Danthewaann <daniel.black@slicelife.com>
2026-01-23 23:09:43 +0000

    Add binds to jump to specific items in harpoon

--- nvim/nvim/lua/plugins/core/harpoon.lua
+++ nvim/nvim/lua/plugins/core/harpoon.lua
@@ -9,6 +9,12 @@ return {
     vim.keymap.set("n", "<leader><space>", function() harpoon:list():add() end, { desc = "Add file to harpoon list" })
     vim.keymap.set("n", "<C-b>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
       { desc = "Toggle Harpoon List" })
+
+    vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Jump to harpoon item 1" })
+    vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Jump to harpoon item 2" })
+    vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Jump to harpoon item 3" })
+    vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Jump to harpoon item 4" })
+    vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Jump to harpoon item 5" })
     vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end, { desc = "Jump to previous harpoon item" })
     vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end, { desc = "Jump to next harpoon item" })
   end
