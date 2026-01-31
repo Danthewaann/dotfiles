bb1e447df2b0c480be355d862602fb157d4bce9c
Danthewaann <daniel.black@slicelife.com>
2026-01-31 13:41:13 +0000

    Only enable dictionary source in markdown and comment blocks

--- nvim/nvim/lua/plugins/core/blink-cmp.lua
+++ nvim/nvim/lua/plugins/core/blink-cmp.lua
@@ -1,3 +1,42 @@
+-- Use this function to check if the cursor is inside a comment block
+-- From: https://github.com/Kaiser-Yang/blink-cmp-dictionary#how-to-enable-this-plugin-for-comment-blocks-or-specific-file-types-only
+local function inside_comment_block()
+  if vim.api.nvim_get_mode().mode ~= "i" then
+    return false
+  end
+  local node_under_cursor = vim.treesitter.get_node()
+  local parser = vim.treesitter.get_parser(nil, nil, { error = false })
+  local query = vim.treesitter.query.get(vim.bo.filetype, "highlights")
+  if not parser or not node_under_cursor or not query then
+    return false
+  end
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  row = row - 1
+  for id, node, _ in query:iter_captures(node_under_cursor, 0, row, row + 1) do
+    if query.captures[id]:find("comment") then
+      local start_row, start_col, end_row, end_col = node:range()
+      if start_row <= row and row <= end_row then
+        if start_row == row and end_row == row then
+          if start_col <= col and col <= end_col then
+            return true
+          end
+        elseif start_row == row then
+          if start_col <= col then
+            return true
+          end
+        elseif end_row == row then
+          if col <= end_col then
+            return true
+          end
+        else
+          return true
+        end
+      end
+    end
+  end
+  return false
+end
+
 return {
   "saghen/blink.cmp",
   event = "VimEnter",
@@ -83,7 +122,19 @@ return {
     },
 
     sources = {
-      default = { "dictionary", "git", "lsp", "path", "snippets", "buffer" },
+      default = function()
+        -- put those which will be shown always
+        local result = { "lsp", "path", "snippets", "buffer", "git" }
+        if
+        -- turn on dictionary in markdown or text file
+            vim.tbl_contains({ "markdown", "text" }, vim.bo.filetype) or
+            -- or turn on dictionary if cursor is in the comment block
+            inside_comment_block()
+        then
+          table.insert(result, "dictionary")
+        end
+        return result
+      end,
 
       providers = {
         -- defaults to `{ 'buffer' }`
