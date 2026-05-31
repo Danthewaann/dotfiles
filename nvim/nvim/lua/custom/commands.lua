23d2c39ee7b4f90754b07c3b622f2fc25349fc43
Danthewaann <daniel.black@slice.com>
2026-05-31 09:09:58 +0100

    Move mypy and ruff code from utils to commands

--- nvim/nvim/lua/custom/commands.lua
+++ nvim/nvim/lua/custom/commands.lua
@@ -3,13 +3,56 @@ local utils = require("custom.utils")
 vim.api.nvim_create_user_command("Mypy", function()
   utils.print("Running mypy...")
   vim.system(
-    utils.mypy_args(true), {}, function(obj)
+    {
+      utils.get_venv_executable_path("mypy"),
+      "--show-column-numbers",
+      "--show-error-end",
+      "--show-error-codes",
+      "--hide-error-context",
+      "--no-color-output",
+      "--no-error-summary",
+      "--no-pretty",
+      "."
+    }, {}, function(obj)
       vim.schedule(function()
         if obj.code > 1 then
           utils.print_err(vim.fn.trim(obj.stderr))
           return
         end
-        utils.parse_mypy_output(obj.stdout)
+
+        -- From: https://github.com/mfussenegger/nvim-lint/blob/master/lua/lint/linters/mypy.lua
+        local output = obj.stdout
+        local pattern = "([^:]+):(%d+):(%d+):(%d+):(%d+): (%a+): (.*) %[(%a[%a-]+)%]"
+        local severities = {
+          error = "E",
+          warning = "W",
+          note = "N",
+        }
+
+        local list = {}
+        for line in vim.gsplit(output, "\n", { plain = true }) do
+          local file, lnum, col, end_lnum, end_col, severity, message, code = line:match(pattern)
+          if file then
+            table.insert(list, {
+              filename = file,
+              lnum = lnum,
+              col = col,
+              end_lnum = end_lnum,
+              end_col = end_col,
+              nr = code,
+              type = severities[severity],
+              text = message .. " # type: ignore [" .. code .. "]",
+              source = "mypy"
+            })
+          end
+        end
+
+        vim.fn.setqflist({}, " ", { title = "Mypy errors", items = list })
+        if #list > 0 then
+          utils.print_err("Mypy found " .. #list .. " error(s)")
+        else
+          utils.print("Mypy finished with no errors")
+        end
       end)
     end)
 end, { desc = "Run Mypy and populate quickfix list with errors" })
@@ -33,7 +76,38 @@ vim.api.nvim_create_user_command("Ruff", function()
           utils.print_err(vim.fn.trim(obj.stderr))
           return
         end
-        utils.parse_ruff_output(obj.stdout)
+
+        -- From: https://github.com/mfussenegger/nvim-lint/blob/master/lua/lint/linters/ruff.lua
+        local output = obj.stdout
+        local severities = {
+          ["F821"] = "E", -- undefined name `name`
+          ["E902"] = "E", -- `IOError`
+          ["E999"] = "E", -- `SyntaxError`
+        }
+
+        local list = {}
+        local results = vim.json.decode(output)
+        for _, result in ipairs(results or {}) do
+          local diagnostic = {
+            filename = result.filename,
+            lnum = result.location.row,
+            col = result.location.column,
+            end_lnum = result.end_location.row,
+            end_col = result.end_location.column,
+            nr = result.code,
+            type = severities[result.code] or "W",
+            text = result.message,
+            source = "ruff"
+          }
+          table.insert(list, diagnostic)
+        end
+
+        vim.fn.setqflist({}, " ", { title = "Ruff errors", items = list })
+        if #list > 0 then
+          utils.print_err("Ruff found " .. #list .. " error(s)")
+        else
+          utils.print("Ruff finished with no errors")
+        end
       end)
     end)
 end, { desc = "Run Ruff and populate quickfix list with errors" })
