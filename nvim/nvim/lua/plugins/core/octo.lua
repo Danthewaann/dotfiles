42afcce72eefb49c8ec910d5a15d117aee1c6d3c
Danthewaann <daniel.black@slice.com>
2026-08-19 08:49:41 +0100

    Remove <localleader>t bind from octo config

--- nvim/nvim/lua/plugins/core/octo.lua
+++ nvim/nvim/lua/plugins/core/octo.lua
@@ -64,6 +64,5 @@ return {
       print("Opening review for PR...")
       vim.cmd("Octo review")
     end, { desc = "Start or resume review" })
-    vim.keymap.set("n", "<localleader>t", "<cmd>Octo review thread<CR>", { desc = "Show review threads" })
   end
 }
