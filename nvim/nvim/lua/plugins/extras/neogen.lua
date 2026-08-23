3ec7792c0e9697cbaa67e7fa778047c52ae3655c
Danthewaann <daniel.black@slice.com>
2026-08-23 11:13:21 +0100

    Move <leader>gd to GenerateDocs command

--- nvim/nvim/lua/plugins/extras/neogen.lua
+++ nvim/nvim/lua/plugins/extras/neogen.lua
@@ -1,14 +1,13 @@
 return {
   "danymat/neogen",
   dependencies = "nvim-treesitter/nvim-treesitter",
-  keys = {
-    {
-      "<leader>gd",
-      function()
-        require("neogen").generate()
-      end,
-      desc = "[G]enerate [D]ocstring"
-    }
-  },
-  opts = { snippet_engine = "luasnip" }
+  cmd = "GenerateDocs",
+  opts = { snippet_engine = "luasnip" },
+  config = function(opts)
+    require("neogen").setup(opts)
+
+    vim.api.nvim_create_user_command("GenerateDocs", function()
+      require("neogen").generate()
+    end, { desc = "Generate docs for code under the cursor" })
+  end
 }
