bd83d3fbc9c663c8c30aebff393d11332c0e2bb6
Danthewaann <daniel.black@slice.com>
2026-08-23 12:40:05 +0100

    Add hidden = true for files, grep and grep_word snacks pickers

--- nvim/nvim/lua/plugins/core/snacks.lua
+++ nvim/nvim/lua/plugins/core/snacks.lua
@@ -109,23 +109,23 @@ return {
   },
   keys = {
     -- core
-    { "<leader>s/", function() Snacks.picker.search_history() end,  desc = "Search History" },
-    { "<leader>:",  function() Snacks.picker.command_history() end, desc = "Command History" },
-    { "<leader>n",  function() Snacks.picker.notifications() end,   desc = "Notification History" },
-    { "<leader>e",  function() Snacks.explorer() end,               desc = "File Explorer" },
-    { "<leader>B",  function() Snacks.picker.pick() end,            desc = "Builtin" },
+    { "<leader>s/", function() Snacks.picker.search_history() end,             desc = "Search History" },
+    { "<leader>:",  function() Snacks.picker.command_history() end,            desc = "Command History" },
+    { "<leader>n",  function() Snacks.picker.notifications() end,              desc = "Notification History" },
+    { "<leader>e",  function() Snacks.explorer() end,                          desc = "File Explorer" },
+    { "<leader>B",  function() Snacks.picker.pick() end,                       desc = "Builtin" },
     -- find
-    { "<C-f>",      function() Snacks.picker.files() end,           desc = "Find Files" },
-    { "<C-p>",      function() Snacks.picker.git_files() end,       desc = "Find Git Files" },
-    { "<leader>,",  function() Snacks.picker.buffers() end,         desc = "Buffers" },
-    { "<leader>.",  function() Snacks.picker.recent() end,          desc = "Recent" },
-    { "<leader>S",  function() Snacks.picker.smart() end,           desc = "Smart Find Files" },
+    { "<C-f>",      function() Snacks.picker.files({ hidden = true }) end,     desc = "Find Files" },
+    { "<C-p>",      function() Snacks.picker.git_files() end,                  desc = "Find Git Files" },
+    { "<leader>,",  function() Snacks.picker.buffers() end,                    desc = "Buffers" },
+    { "<leader>.",  function() Snacks.picker.recent() end,                     desc = "Recent" },
+    { "<leader>S",  function() Snacks.picker.smart() end,                      desc = "Smart Find Files" },
     -- main search
-    { "<leader>sb", function() Snacks.picker.lines() end,           desc = "Buffer Lines" },
-    { "<leader>sB", function() Snacks.picker.grep_buffers() end,    desc = "Grep Open Buffers" },
-    { "<leader>/",  function() Snacks.picker.grep() end,            desc = "Grep" },
-    { "<leader>sw", function() Snacks.picker.grep_word() end,       desc = "Visual selection or word", mode = { "n", "x" } },
-    { "<leader>si", function() Snacks.picker.icons() end,           desc = "Icons" },
+    { "<leader>sb", function() Snacks.picker.lines() end,                      desc = "Buffer Lines" },
+    { "<leader>sB", function() Snacks.picker.grep_buffers() end,               desc = "Grep Open Buffers" },
+    { "<leader>/",  function() Snacks.picker.grep({ hidden = true }) end,      desc = "Grep" },
+    { "<leader>sw", function() Snacks.picker.grep_word({ hidden = true }) end, desc = "Visual selection or word", mode = { "n", "x" } },
+    { "<leader>si", function() Snacks.picker.icons() end,                      desc = "Icons" },
     {
       "<leader>sd",
       function()
